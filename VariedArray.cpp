9eb56d0c345a403773fefa4d80358ec4b6cce744
Cavan Theiss <cavanrt@gmail.com>
2025-07-10 16:57:28 -0500

    Update VariedArray.cpp
    Finally figured it out, here is my new completely original version.
    
    does the same shit, just this version was all mine and so that makes it better.

--- VariedArray.cpp
+++ VariedArray.cpp
@@ -1,37 +1,32 @@
 #include <vector>
 #include <iostream>
 using namespace std;
-// This program is pissing me off, because I can't figure out how it's coming up with the outputs for the inputs.
 
 
 int main() {
-
-        int a,b,x;
-        cin>>a>>b;
-        int* arr[a];
-        int c[b][2],d[a];
-        vector<int> v;
-        for(int i=0;i<a;i++){
-                int k;
-                cin>>k;
-                for(int j=0;j<k;j++){
-                        cin>>x;
-                        v.push_back(x);
-                }
-                d[i]=k;
-                }
-        for(int k=0;k<b;k++){
-                cin>>(c[k][0])>>(c[k][1]);
+    
+    int rows, queries;
+    cin >> rows >> queries;
+    
+    // Vector of vectors to hold variable length arrays
+    vector<vector<int>> arr(rows);
+            
+    // For loop for creating rows
+    for(int i = 0; i < rows; i++){
+        int items;
+        cin >> items;
+        arr[i].resize(items);
+    // For loop for creating space for and stocking items
+        for(int j = 0; j < items; j++){
+            cin >> arr[i][j];
         }
-        int* p = &v[0];
-        for(int i=0;i<a;i++){
-            arr[i]=p;
-            p += d[i]; 
-        }
-        for(int i=0;i<b;i++){
-            cout<<*(arr[c[i][0]] + c[i][1])<<endl;
-        }
-
-                             
+    } 
+    // For loop for running through the search queries
+    for(int i = 0; i < queries; i++){
+        int x, y;
+        cin >> x >> y;
+        cout << arr[x][y] << endl;
+    }
     return 0;
     }
+
