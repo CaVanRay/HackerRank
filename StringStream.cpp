d5f98dc2321fb3bb261550d78fb423d887b70d4d
Cavan Theiss <cavanrt@gmail.com>
2025-07-11 23:23:32 -0500

    Update StringStream.cpp

--- StringStream.cpp
+++ StringStream.cpp
@@ -1,1 +1,26 @@
+#include <sstream>
+#include <vector>
+#include <iostream>
+using namespace std;
 
+vector<int> parseInts(string str) {
+	// Complete this function
+    vector<int> res;
+    string token;
+    stringstream ss(str);
+    while(getline(ss,token,',')){
+        res.push_back(stoi(token));
+    }
+    return res;
+}
+
+int main() {
+    string str;
+    cin >> str;
+    vector<int> integers = parseInts(str);
+    for(int i = 0; i < integers.size(); i++) {
+        cout << integers[i] << "\n";
+    }
+    
+    return 0;
+}
