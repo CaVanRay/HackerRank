b96db75e075eee98c1af39cdfc3df93fda591532
Cavan Theiss <cavanrt@gmail.com>
2025-07-13 21:04:21 -0500

    Update StringStream.cpp
    I have a better understanding of how this works now, updated with my own version.
    
    includes comments explaining wtf is going on

--- StringStream.cpp
+++ StringStream.cpp
@@ -3,24 +3,49 @@
 #include <iostream>
 using namespace std;
 
-vector<int> parseInts(string str) {
-	// Complete this function
-    vector<int> res;
-    string token;
-    stringstream ss(str);
-    while(getline(ss,token,',')){
-        res.push_back(stoi(token));
+
+//Function for seperating string into parts and converting into integers
+vector<int> parser(string input_string) {
+    
+    //create vector "result" for returning integers to main
+    vector<int> result;
+    
+    //create string "chunk" for handling split off sections of input_stream
+    string chunk;
+    
+    //create stringstream "input_stream" for treating input str as a stream of data
+    stringstream input_stream(input_string);
+    
+    //getline takes "input_stream" breaks it into "chunk"s using ',' to tell the different sections apart
+    //as long as getline() recieves more data, the while() function returns true
+    while(getline( input_stream, chunk,',')){
+        
+        //"result" is the vector we created at the beginning
+        //".push_back" adds item to vector and increases its size
+        //"stoi()" is a simple "string to int" tool for converting a string of numbers to actual integers
+        result.push_back(stoi(chunk));
     }
-    return res;
+    
+    // we now return the whole vector of numbers from the input string back to main()
+    return result;
 }
 
 int main() {
-    string str;
-    cin >> str;
-    vector<int> integers = parseInts(str);
+    
+    // First declare and then take in the original string of numbers
+    string input_string;
+    cin >> input_string;
+    
+    // Declare a vecter that takes the results of the parser funtion as input
+    vector<int> integers = parser(input_string);
+    
+    // Finally make a for() loop that tests against the size of the vector
     for(int i = 0; i < integers.size(); i++) {
+        
+        // each loop outputs the current item in the vector
         cout << integers[i] << "\n";
     }
     
     return 0;
 }
+// Boom motherfucker
