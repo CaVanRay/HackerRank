ffba71c7452c56acf0dd3ba6071789bfbfb94961
Cavan Theiss <cavanrt@gmail.com>
2025-07-14 13:43:34 -0500

    Update AttributeParser.cpp
    This isn't my code, but it does exactly what I need so I'm saving my space first and then will go back to translate, understand, & re-write

--- AttributeParser.cpp
+++ AttributeParser.cpp
@@ -1,12 +1,67 @@
-#include <cmath>
-#include <cstdio>
-#include <vector>
 #include <iostream>
-#include <algorithm>
+#include <vector>
+#include <bits/stdc++.h>
+
 using namespace std;
 
-
 int main() {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    
+    int n, q;
+    cin>>n>>q;
+    cin.ignore();
+    
+    map<string, map<string, string>> hrml;
+    string line, path = "";
+    
+    while (n-->0) {
+        getline(cin, line);
+        stringstream ss(line);
+        
+        string token;
+        ss >> token;
+        
+        if(token[1]=='/'){
+            size_t pos = path.rfind('.');
+            if(pos!=string::npos){
+                path = path.substr(0, pos);
+            }else{
+                path = "";
+            }
+        }else{
+            string tag = token.substr(1);
+            if(!tag.empty() && tag.back() == '>') tag.pop_back();
+            
+            if(!path.empty()){
+                path += "." + tag;
+            }else{
+                path = tag;
+            }
+            
+            string attr, eq, value;
+            while (ss >> attr >> eq >> value) {    
+                
+                if(value.front()=='"'){
+                    if(!value.empty() && value.back() == '>') value.pop_back();
+                    value = value.substr(1, value.size() - 2);
+                }
+                 
+                hrml[path][attr]=value;
+            }
+        }
+    }
+    
+    while (q-->0) {
+        getline(cin, line);
+        size_t tilPos = line.find('~');
+        string tagPath = line.substr(0,tilPos);
+        string attr = line.substr(tilPos + 1);
+        
+        if(hrml.count(tagPath) && hrml[tagPath].count(attr)){
+            cout<<hrml[tagPath][attr]<<endl;
+        }else{
+            cout<<"Not Found!"<<endl;
+        }
+    }
+       
     return 0;
 }
