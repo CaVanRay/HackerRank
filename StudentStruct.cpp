7f6aefd9692f4bd9c338e7bdafa39f8a3a9bdd03
Cavan Theiss <cavanrt@gmail.com>
2025-07-22 16:48:01 -0500

    Update StudentStruct.cpp
    added basic struct

--- StudentStruct.cpp
+++ StudentStruct.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 using namespace std;
 
+struct student{
+    int age;
+    string fname;
+    string lname;
+    int stands;
+    
+    
+    
+}
+
 
 int main() {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
