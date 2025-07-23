d8f8c29c77b98ae8dc61833e84fdc2d461fdcb18
Cavan Theiss <cavanrt@gmail.com>
2025-07-23 08:58:23 -0500

    Update StudentStruct.cpp
    this one is 100% mine, I didn't need to pull any little bit from anywhere, I made this from start to finish without having to look anything up at all.

--- StudentStruct.cpp
+++ StudentStruct.cpp
@@ -1,21 +1,28 @@
 #include <iostream>
 using namespace std;
 
-struct student{
+
+struct Student{
     int age;
     string fname;
     string lname;
-    int stands;
-    
-    
-    
-}
+    int stands; 
+};
 
 
 int main() {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
     
+    Student student;   
     
+    cin >> student.age;
+    cin >> student.fname;
+    cin >> student.lname;
+    cin >> student.stands;
+    
+    cout << student.age << " " ; 
+    cout << student.fname << " " ;
+    cout << student.lname << " " ;
+    cout << student.stands << endl;
      
     return 0;
 }
