ab4685862204e571b3faec11a886aacfdfbe6590
Cavan Theiss <cavanrt@gmail.com>
2025-07-30 13:23:43 -0500

    Update ClassAndObjects.cpp
    Wrote it all but had help tracking down a logic error

--- ClassAndObjects.cpp
+++ ClassAndObjects.cpp
@@ -1,8 +1,42 @@
 #include <iostream>
 using namespace std;
 
+class Student{
+    
+    int scores[5];
+    
+    public:
+    
+    void input(){
+        for(int i = 0; i < 5; i++){
+            cin >> scores[i];
+        }
+    };
+    
+   int calculateTotalScore(){
+        int sum = 0;
+        for(int j = 0; j < 5; j++){
+            sum = sum + scores[j];
+        }
+        return sum;
+    };
+};
 
 int main() {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    
+    int NumOfStuds, firstScore, BettaThanU = 0;
+    cin >> NumOfStuds;
+    Student Stud[NumOfStuds];
+    for(int k = 0; k < NumOfStuds; k++){
+        int Score = 0;
+        Stud[k].input();
+        Score = Stud[k].calculateTotalScore();
+        if(k == 0){
+            firstScore = Score;
+        }else if(Score > firstScore){
+            BettaThanU++;
+            }   
+    }
+    cout << BettaThanU;
     return 0;
 }
