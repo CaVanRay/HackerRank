7366c7a3f20432fbe85aa6f4cf40318bb66687d2
Cavan Theiss <cavanrt@gmail.com>
2025-08-03 21:25:16 -0500

    Update AttendingWorkshops.cpp

--- AttendingWorkshops.cpp
+++ AttendingWorkshops.cpp
@@ -4,7 +4,7 @@ using namespace std;
 //************************************************************************************************
 //Define the structs Workshops and Available_Workshops.
 //Implement the functions initialize and CalculateMaxWorkshops
-
+/*
 struct Workshops{
 int Start, Dur, End;
 };
@@ -14,6 +14,63 @@ struct Available_Workshops(int start_time[], int duration[], int n){
 Workshops Signedup[n];
 
 };
+*/
+
+
+struct Workshop{
+    int starttime = 0 , duration = 0, endtime = 0;
+    
+    Workshop(int _startime, int _duration){
+        starttime = _startime;
+        duration = _duration;
+        endtime = _startime + _duration;
+    }
+    
+};
+
+struct Available_Workshops{
+    int len = 0;
+    vector<Workshop> workshops;
+    
+    Available_Workshops(int _len, vector<Workshop> _workshops){
+        len=_len;
+        workshops=_workshops;
+    }
+
+};
+
+Available_Workshops* initialize(int* startime, int* duration, int n){
+    vector<Workshop> workshops;
+        
+    for (int i = 0; i<n; i++) {
+        workshops.emplace_back(startime[i], duration[i]);      
+    }
+        
+    return new Available_Workshops(n, workshops);
+}
+
+int CalculateMaxWorkshops(Available_Workshops* ptr){
+    int current_end = 0, scheduled = 0;
+
+    vector<Workshop> workshops = ptr->workshops;
+    
+    sort(workshops.begin(), workshops.end(), [](const Workshop& a, const Workshop& b) {
+        return a.endtime < b.endtime;
+    });
+    
+    for (Workshop& w : workshops){
+        if (w.starttime >= current_end) {
+            scheduled++;
+            current_end = w.endtime;
+        }
+    }
+    
+    return scheduled;
+}
+
+
+
+
 //************************************************************************************************
 int main(int argc, char *argv[]) {
     int n; // number of workshops
