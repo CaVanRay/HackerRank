4b09c632c7d1b86b92f6edce7010ba476c4d0500
Cavan Theiss <cavanrt@gmail.com>
2025-08-07 13:53:02 -0500

    Update BoxItClass.cpp

--- BoxItClass.cpp
+++ BoxItClass.cpp
@@ -31,12 +31,80 @@ class Box{
     };
     
     // Operator overloading <
-    
+    bool operator<(const Box& boox){
+        if (l < boox.l)
+            return true;
+        else if (l == boox.l && b < boox.b)
+            return true;
+        else if (l == boox.l && b == boox.b && h < boox.h)
+            return true;
+        else
+            return false;
+    }
+
+    // Operator overloading <<
+    friend ostream& operator<<(ostream& out, const Box& Boox) {
+        out << Boox.l << " " << Boox.b << " " << Boox.h;
+        return out;
+    }
 };
 
-int main() {
-    
-    
-    
-    return 0;
+
+/* 
+Everything from here down is bs, this whole section was supposed to be included in the excersize but wasnt, I had to google it.
+
+If i was supposed to figure out how to write it then the instructions a messed up or straight up got forgotten.
+
+*/
+void check2()
+{
+    int n;
+    cin>>n;
+    Box temp;
+    for(int i=0;i<n;i++)
+    {
+        int type;
+        cin>>type;
+        if(type ==1)
+        {
+            cout<<temp<<endl;
+        }
+        if(type == 2)
+        {
+            int l,b,h;
+            cin>>l>>b>>h;
+            Box NewBox(l,b,h);
+            temp=NewBox;
+            cout<<temp<<endl;
+        }
+        if(type==3)
+        {
+            int l,b,h;
+            cin>>l>>b>>h;
+            Box NewBox(l,b,h);
+            if(NewBox<temp)
+            {
+                cout<<"Lesser\n";
+            }
+            else
+            {
+                cout<<"Greater\n";
+            }
+        }
+        if(type==4)
+        {
+            cout<<temp.CalculateVolume()<<endl;
+        }
+        if(type==5)
+        {
+            Box NewBox(temp);
+            cout<<NewBox<<endl;
+        }
+
+    }
+}
+
+int main()
+{
+    check2();
 }
