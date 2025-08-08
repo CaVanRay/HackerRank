a792fc436c7c45ac9f50e3a09305ac1b46df5b66
Cavan Theiss <cavanrt@gmail.com>
2025-08-08 15:51:49 -0500

    Update InheritedCode.cpp

--- InheritedCode.cpp
+++ InheritedCode.cpp
@@ -10,17 +10,32 @@ using namespace std;
 
 //BadLengthException comes after the checkUsername section.
 class BadLengthException {
+     private: 
+        int n;
+    public:
+        BadLengthException(int errornumber) {
+            n = errornumber;
+        }
     
-    
-    
+        int what() {
+            return n;
+        }
 }
 
 // This is just checking the validity of the username presented
 // If invalidity is specifically the length, then BadLengthException is run
 bool checkUsername(string username) {
-    
-    
-    
+    bool isValid = true;
+    int n = username.length();
+    if(n < 5) {
+        throw BadLengthException(n);
+    }
+    for(int i = 0; i < n-1; i++) {
+        if(username[i] == 'w' && username[i+1] == 'w') {
+            isValid = false;
+        }
+    }
+    return isValid;
 }
 
 //Apparently this was already supposed to be here?
@@ -34,13 +49,14 @@ int main() {
         try {
             bool isValid = checkUsername(username);
             if(isValid) {
-                cout << "Valid" << 'n';
+                cout << "Valid" << endl;
             } else {
-                cout << "Invalid" << 'n';
+                cout << "Invalid" << endl;
             }
         } catch (BadLengthException e) {
-            cout << "Too short: " << e.what() << 'n';
+            cout << "Too short: " << e.what() << endl;
         }
     }
     return 0;
 }
+
