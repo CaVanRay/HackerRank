f81d251f280259f33f66b64857ad80e0ff090edc
Cavan Theiss <cavanrt@gmail.com>
2025-08-08 13:28:42 -0500

    Update InheritedCode.cpp

--- InheritedCode.cpp
+++ InheritedCode.cpp
@@ -1,31 +1,46 @@
 #include <iostream>
 #include <string>
 #include <sstream>
+#include <exception>
 
 using namespace std;
+//**********************************************************************************
+// OK, I now know what they want from me & I'm fairly confident that Ill be able to write this up.
 
 
+//BadLengthException comes after the checkUsername section.
+class BadLengthException {
+    
+    
+    
+}
+
+// This is just checking the validity of the username presented
+// If invalidity is specifically the length, then BadLengthException is run
+bool checkUsername(string username) {
+    
+    
+    
+}
+
+//Apparently this was already supposed to be here?
+//I need to look into why my excersizes are starting with the wrong default code
+//**********************************************************************************
 int main() {
-    
-    int t;
-    cin >> t;
-    for(int i = 0; i < t; i++){
-    
-    string uname;
-    cin >> uname;
-    size_t stringlength = uname.length();
-    if (stringlength < 3) {
-        cout << "Too short: " << stringlength << endl;
-    } else if (uname == "Peter"){
-        cout << "Valid"  << endl;
-    }   else {
-        cout << "Invalid" << endl;
+    int T; cin >> T;
+    while(T--) {
+        string username;
+        cin >> username;
+        try {
+            bool isValid = checkUsername(username);
+            if(isValid) {
+                cout << "Valid" << 'n';
+            } else {
+                cout << "Invalid" << 'n';
+            }
+        } catch (BadLengthException e) {
+            cout << "Too short: " << e.what() << 'n';
+        }
     }
-    
-    
-    }
-    
-    
-       
     return 0;
 }
