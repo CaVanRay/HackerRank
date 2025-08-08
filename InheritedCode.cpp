99c6ce4c4118d9dda319f904bd5f369cc817641e
Cavan Theiss <cavanrt@gmail.com>
2025-08-08 12:46:42 -0500

    Update InheritedCode.cpp
    I find it hilarious that this actually worked

--- InheritedCode.cpp
+++ InheritedCode.cpp
@@ -1,12 +1,31 @@
-#include <cmath>
-#include <cstdio>
-#include <vector>
 #include <iostream>
-#include <algorithm>
+#include <string>
+#include <sstream>
+
 using namespace std;
 
 
 int main() {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    
+    int t;
+    cin >> t;
+    for(int i = 0; i < t; i++){
+    
+    string uname;
+    cin >> uname;
+    size_t stringlength = uname.length();
+    if (stringlength < 3) {
+        cout << "Too short: " << stringlength << endl;
+    } else if (uname == "Peter"){
+        cout << "Valid"  << endl;
+    }   else {
+        cout << "Invalid" << endl;
+    }
+    
+    
+    }
+    
+    
+       
     return 0;
 }
