f2bd5b85d63dc81199f0f4a6c98aba763da0ac94
Cavan Theiss <cavanrt@gmail.com>
2025-08-09 18:10:21 -0500

    Create TemplateSpecialization.cpp

---
+++ TemplateSpecialization.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+enum class Fruit { apple, orange, pear };
+enum class Color { red, green, orange };
+
+template <typename T> struct Traits;
+
+// Define specializations for the Traits class template here.
+
