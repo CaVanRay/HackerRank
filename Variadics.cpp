062974d099d08ca67315535336736ed5e737bf00
Cavan Theiss <cavanrt@gmail.com>
2025-08-15 07:53:52 -0500

    Update Variadics.cpp

--- Variadics.cpp
+++ Variadics.cpp
@@ -1,9 +1,20 @@
-
 #include <iostream>
 using namespace std;
 
 // Enter your code for reversed_binary_value<bool...>()
-
+template<bool...bits>
+int reversed_binary_value(){
+    
+    bool binary[] = {bits...};
+    int dec = 0, base = 1;
+    
+    for(bool bit : binary){
+        dec += bit*base;
+        base*=2;
+    }
+    
+    return dec;
+}
 
 template <int n, bool...digits>
 struct CheckValues {
