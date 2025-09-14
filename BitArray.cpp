62e34f829850843344440e68a1ea46ddf1999e0f
Cavan Theiss <cavanrt@gmail.com>
2025-09-14 14:18:40 -0500

    Update BitArray.cpp

--- BitArray.cpp
+++ BitArray.cpp
@@ -6,6 +6,11 @@
 using namespace std;
 
 // This one should be pretty easy, reading up on the logic now.
+/*
+I hate when they make questions more convoluted than they need to be on purpose. 
+Just because a question is easy to answer doesnt mean you should go out of your way to make it difficult to understand.
+
+*/
 
 int main() {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
