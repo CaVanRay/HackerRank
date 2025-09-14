4c55b0a7487a95dacff9eea2ac930ba145bd5de8
Cavan Theiss <cavanrt@gmail.com>
2025-09-14 14:21:44 -0500

    Update BitArray.cpp

--- BitArray.cpp
+++ BitArray.cpp
@@ -10,6 +10,17 @@ using namespace std;
 I hate when they make questions more convoluted than they need to be on purpose. 
 Just because a question is easy to answer doesnt mean you should go out of your way to make it difficult to understand.
 
+I got chatgpt to translate what they want:
+
+You’re given four numbers (N, S, P, Q). Use them to make a sequence with this rule:
+
+First number is S mod 2^31.
+
+Each next number = (previous * P + Q) mod 2^31.
+
+Now, from the first N numbers you generate, just count how many are different and print that number.
+
+
 */
 
 int main() {
