a2f7e98f1e21bbf089b761348a931fe3e4b34e03
Cavan Theiss <cavanrt@gmail.com>
2025-09-20 15:23:23 -0500

    Update Vector-Sort.cpp

--- Vector-Sort.cpp
+++ Vector-Sort.cpp
@@ -1,29 +1,26 @@
-#include <cmath>
-#include <cstdio>
 #include <vector>
 #include <iostream>
 #include <algorithm>
 using namespace std;
 
-/*
-you are given N numbers, sort them and output
 
-example:
-
-5
-28563
-----
-23568
-
-*/
+    // sortingBin.push_back(tempInt); pushing integer into vector
+    // sortingBin.pop_back(tempInt); popping the last element from vector (size decreases by 1)
+    // sort(sortingBin.begin(), sortingBin.end()); (Will sort all the elements in the vector)
+        
 
 int main() {
     
-    int count;
+    int count, tempInt;
     cin >> count;
     vector<int>sortingBin;
     for(int i = 0; i < count; i++){
-        
+        cin >> tempInt;
+        sortingBin.push_back(tempInt);
+    }
+    sort(sortingBin.begin(), sortingBin.end());
+    for (int i = 0; i < count; i++) {
+        cout << sortingBin[i] << " ";
     }
     return 0;
 }
