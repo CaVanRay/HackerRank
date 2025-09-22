5fac27f8816d91c206242c73dfff5b1fba311928
Cavan Theiss <cavanrt@gmail.com>
2025-09-21 20:59:16 -0500

    Update Vector-Erase.cpp

--- Vector-Erase.cpp
+++ Vector-Erase.cpp
@@ -1,4 +1,3 @@
-
 #include <cmath>
 #include <cstdio>
 #include <vector>
@@ -6,18 +5,18 @@
 #include <algorithm>
 using namespace std;
 
-/*
-
-Removes the element present at position.  
-Ex: v.erase(v.begin()+4); (erases the fifth element of the vector v)
-
-Removes the elements in the range from start to end inclusive of the start and exclusive of the end.
-Ex:v.erase(v.begin()+2,v.begin()+5);(erases all the elements from the third element to the fifth element.)
-
-*/
-
 
 int main() {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    
+    int n, temp, removeUno, removeFrom, removeTo;
+    vector<int> Shrinky;
+    cin >> n;
+    
+    for(int i = 0; i < n; i++){
+        cin >> temp;
+        Shrinky.push_back(temp);
+    }
+    cin >> removeUno;
+    
     return 0;
 }
