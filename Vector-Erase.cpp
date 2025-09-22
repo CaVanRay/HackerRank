2f184bbc8674a52acb558a154cf612680b304162
Cavan Theiss <cavanrt@gmail.com>
2025-09-21 21:00:25 -0500

    Update Vector-Erase.cpp

--- Vector-Erase.cpp
+++ Vector-Erase.cpp
@@ -5,6 +5,15 @@
 #include <algorithm>
 using namespace std;
 
+/*
+
+Removes the element present at position.  
+Ex: v.erase(v.begin()+4); (erases the fifth element of the vector v)
+
+Removes the elements in the range from start to end inclusive of the start and exclusive of the end.
+Ex:v.erase(v.begin()+2,v.begin()+5);(erases all the elements from the third element to the fifth element.)
+
+*/
 
 int main() {
     
