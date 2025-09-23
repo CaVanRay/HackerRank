f1115fcbe1ed561c450abc26b971ece3a00a946b
Cavan Theiss <cavanrt@gmail.com>
2025-09-23 11:29:32 -0500

    Update Non-DiviSubs.cpp

--- Non-DiviSubs.cpp
+++ Non-DiviSubs.cpp
@@ -20,6 +20,9 @@ vector<string> split(const string &);
 // try to find every pair that when added together cannot be divided by K 
 // at the end return the largest size of numbers collected that cannot be added with each other and divided by k
 
+// instead of removing what fails, what if I add what succeeds?
+// start with nothing and slowly build up
+
 
 int nonDivisibleSubset(int k, vector<int> s) {
 
