18511e0a10f86e5e3a8b95a26d0b948f9f8f325f
Cavan Theiss <cavanrt@gmail.com>
2025-10-01 13:24:40 -0500

    Update LowerBoundSTL.cpp

--- LowerBoundSTL.cpp
+++ LowerBoundSTL.cpp
@@ -4,9 +4,15 @@ using namespace std;
 
 /*
 
-The first line of the input contains the number of integers . The next line contains  integers in sorted order. The next line contains , the number of queries. Then  lines follow each containing a single integer .
-
-Note: If the same number is present multiple times, you have to print the first index at which it occurs. Also, the input is such that you always have an answer for each query. **CLEAN THIS UP TOMORROW**
+input N
+for(N) -> array
+input Q
+for(Q) -> search for K
+    for(N)
+        if < k = keep going
+        if == k = "yes", output location, break
+        if > k = "No", output location, break
+        
 
 */
 
