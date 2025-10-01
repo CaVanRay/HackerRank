6f34e95426709fbde8b75b39ed9d0664867425f9
Cavan Theiss <cavanrt@gmail.com>
2025-10-01 16:05:34 -0500

    Update Sets-STL.cpp

--- Sets-STL.cpp
+++ Sets-STL.cpp
@@ -1,1 +1,36 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
 
+/*
+
+you will be given  queries. Each query is of one of the following three types:
+
+    Add an element  to the set.
+  
+    Delete an element  from the set. (If the number  is not present in the set, then do nothing).
+  
+    If the number  is present in the set, then print "Yes"(without quotes) else print "No"(without quotes).
+
+Here are some of the frequently used member functions of sets:
+
+      set<int>s; //Creates a set of integers.
+    
+      int length=s.size(); //Gives the size of the set.
+      
+      s.insert(x); //Inserts an integer x into the set s.
+      
+      s.erase(val); //Erases an integer val from the set s.
+      
+      set<int>::iterator itr=s.find(val); //Gives the iterator to the element val if it is found otherwise returns s.end() .
+  Ex: set<int>::iterator itr=s.find(100); //If 100 is not present then it==s.end().
+
+*/
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    return 0;
+}
