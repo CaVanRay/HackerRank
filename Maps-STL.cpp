4eb1327f3980b2afa2e6d91ab00c93da96023644
Cavan Theiss <cavanrt@gmail.com>
2025-10-03 08:58:59 -0500

    Update Maps-STL.cpp

--- Maps-STL.cpp
+++ Maps-STL.cpp
@@ -7,20 +7,32 @@ int main() {
 int rounds, type, grade;
 string name;
 map<string, int> Students;
+map<string, int>::iterator Clue;
 
 cin >> rounds;
     
     while(rounds--){
-        cin >> type;
+        cin >> type >> name;
+        Clue = Students.find(name);
         switch(type){
             case 1: //Add
-
+                cin >> grade;
+                
+                if(Clue != Students.end()){
+                    Students[name] = Students[name] + grade;
+                }else{
+                    Students.insert(make_pair(name, grade));
+                }
             break;
             case 2: //Delete
-
+                Students[name] = 0;
             break;
-            case 3: //Search
-
+            case 3: //Output
+                if(Clue != Students.end()){
+                    cout << Students[name] << endl;
+                }else{
+                    cout << 0 << endl;
+                }
             break;
         }
     }
