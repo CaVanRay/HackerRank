a7f8dc6503675c3d19f2578b6e19501beae68c1a
Cavan Theiss <cavanrt@gmail.com>
2025-10-03 11:10:55 -0500

    Update PrintPretty.cpp

--- PrintPretty.cpp
+++ PrintPretty.cpp
@@ -12,7 +12,18 @@ int main() {
         double A, B, C;
         cin >> A >> B >> C;
         
+        // Strip, Hex notation, lowercase
         
+        out << A << endl;
+        
+        // Scale to 2 decimal, precede with + or -, right justified & left padded
+        // Printed result is exactly 15 characters wide total
+        
+        out << B << endl;
+        
+        // 9 decimal places, Scientific notation, Uppercase
+        
+        out << C << endl;
             
     }
     return 0;
