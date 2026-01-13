efe4ce3df8db5ffe756cbf5202005d17f6360a53
Cavan Theiss <cavanrt@gmail.com>
2026-01-13 13:25:48 -0600

    Update MaximumElement.cpp

--- MaximumElement.cpp
+++ MaximumElement.cpp
@@ -20,7 +20,7 @@ vector<int> getMax(vector<string> operations) {
 // initially handed string vector OPS (operations)
 // Take size of operation vector and use that to make input testing loop
 
-size_t commands;
+size_t commands = operations.size();
     
 // Operations processing handled in int vector Stack
 
