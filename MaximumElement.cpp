7cc83f90abc6481878d7cd69a231627de54d5e7d
Cavan Theiss <cavanrt@gmail.com>
2026-01-15 15:46:31 -0600

    Update MaximumElement.cpp

--- MaximumElement.cpp
+++ MaximumElement.cpp
@@ -42,6 +42,7 @@ for(size_t i = 0; i < commands; i++){
         
     }else if(temp[0] == '3'){
         // if the first char in the string is 3, we need to take the largest number in the stack and add it to the vector RES
+        // track current largest number, it will be faster than searching for the largest number everytime
         
     }else if(temp.empty()) continue;
     
