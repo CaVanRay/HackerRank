74ff4914c3ebeb0d766170532bf4ab42223cf189
Cavan Theiss <cavanrt@gmail.com>
2026-01-21 15:57:59 -0600

    Update BalancedBrackets.cpp

--- BalancedBrackets.cpp
+++ BalancedBrackets.cpp
@@ -1,12 +1,4 @@
 //*********************************************************************
-
-/*
- * Complete the 'isBalanced' function below.
- *
- * The function is expected to return a STRING.
- * The function accepts STRING s as parameter.
- */
-
 string isBalanced(string s) {
   string theAnswer;
   // Take s length, if balanced string is balanced then every opening bracket
