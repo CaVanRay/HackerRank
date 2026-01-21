0a459d9396ffdbe6481d33ad33d93c39b0629842
Cavan Theiss <cavanrt@gmail.com>
2026-01-21 15:57:45 -0600

    Update BalancedBrackets.cpp

--- BalancedBrackets.cpp
+++ BalancedBrackets.cpp
@@ -10,6 +10,8 @@
 string isBalanced(string s) {
   string theAnswer;
   // Take s length, if balanced string is balanced then every opening bracket
+  // - should have a matching closing bracket
+  
   // If length is odd - you're done, theAnswer = "No"
   // If length even - continue
   
