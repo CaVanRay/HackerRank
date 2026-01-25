847af44d54c5e9410c601ea7c6a8319ea3c2f35f
Cavan Theiss <cavanrt@gmail.com>
2026-01-24 19:59:17 -0600

    Update EqualStacks.cpp

--- EqualStacks.cpp
+++ EqualStacks.cpp
@@ -14,21 +14,28 @@ size_t getSize(const vector<int>& cylinder){
 
 int equalStacks(vector<int> h1, vector<int> h2, vector<int> h3) {
 
-  std::size_t tH1 = getSize(h1), tH2 = getSize(h2), tH3 = getSize(h3);  
+    size_t tH1 = getSize(h1);
+    size_t tH2 = getSize(h2);
+    size_t tH3 = getSize(h3);
 
-  while(!h1.empty() && !h2.empty() && !h3.empty()){
-    if(tH1 == tH2 && tH2 == tH3){
-      return tH1;
-    } else if (tH1 > tH2 && tH1 > tH3){
-      h1.erase(h1.begin());
-    } else if (tH2 > tH1 && tH2 > tH3){
-      h2.erase(h2.begin());
-    } else if (tH3 > tH1 && tH3 > tH2){
-      h3.erase(h3.begin());
+    while (!h1.empty() && !h2.empty() && !h3.empty()) {
+
+        if (tH1 == tH2 && tH2 == tH3) {
+            return static_cast<int>(tH1);
+        }
+
+        if (tH1 >= tH2 && tH1 >= tH3) {
+            tH1 -= h1.front();
+            h1.erase(h1.begin());
+        } else if (tH2 >= tH1 && tH2 >= tH3) {
+            tH2 -= h2.front();
+            h2.erase(h2.begin());
+        } else {
+            tH3 -= h3.front();
+            h3.erase(h3.begin());
+        }
     }
-    tH1 = getSize(h1);
-    tH2 = getSize(h2);
-    tH3 = getSize(h3);
-  }
+
+    return 0;
 }
 //*********************************************************************
