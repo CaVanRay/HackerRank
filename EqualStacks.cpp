109419d2a830d98effc4bce5a2c0ab9ebfe4aa7c
Cavan Theiss <cavanrt@gmail.com>
2026-01-24 18:24:50 -0600

    Update EqualStacks.cpp

--- EqualStacks.cpp
+++ EqualStacks.cpp
@@ -1,5 +1,7 @@
 //*********************************************************************
 int equalStacks(vector<int> h1, vector<int> h2, vector<int> h3) {
 
+  // Record the total height for each stack
+  
 }
 //*********************************************************************
