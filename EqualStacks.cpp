2112a1da2ced30457127e6e1e3958c6d32fa6124
Cavan Theiss <cavanrt@gmail.com>
2026-01-24 18:27:54 -0600

    Update EqualStacks.cpp

--- EqualStacks.cpp
+++ EqualStacks.cpp
@@ -7,6 +7,8 @@ int equalStacks(vector<int> h1, vector<int> h2, vector<int> h3) {
   // Find the highest tH & remove the top cylinder from the associated stack
 
   // Compare tH from each stack
+
+  // if all tH are equal, return height
   
 }
 //*********************************************************************
