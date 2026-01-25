2477f1a36a7428b404947a4f5c3463efe5bc38ba
Cavan Theiss <cavanrt@gmail.com>
2026-01-24 18:28:34 -0600

    Update EqualStacks.cpp

--- EqualStacks.cpp
+++ EqualStacks.cpp
@@ -9,6 +9,7 @@ int equalStacks(vector<int> h1, vector<int> h2, vector<int> h3) {
   // Compare tH from each stack
 
   // if all tH are equal, return height
+  // if not matching, go back to top of loop
   
 }
 //*********************************************************************
