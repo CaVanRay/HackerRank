c970e8e7276139f73f1fdc203c9e3adbd89e3be5
Cavan Theiss <cavanrt@gmail.com>
2026-01-24 19:03:09 -0600

    Update EqualStacks.cpp

--- EqualStacks.cpp
+++ EqualStacks.cpp
@@ -1,4 +1,10 @@
 //*********************************************************************
+int getSize(const vector& cylinder){
+
+
+  
+}
+
 int equalStacks(vector<int> h1, vector<int> h2, vector<int> h3) {
 
   // Record the total height for each stack
