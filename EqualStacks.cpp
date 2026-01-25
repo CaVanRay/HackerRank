d1f41f8a0339ec8a07d7ba034b59ead0dac46f5f
Cavan Theiss <cavanrt@gmail.com>
2026-01-24 19:53:49 -0600

    Update EqualStacks.cpp

--- EqualStacks.cpp
+++ EqualStacks.cpp
@@ -16,15 +16,15 @@ int equalStacks(vector<int> h1, vector<int> h2, vector<int> h3) {
 
   std::size_t tH1 = getSize(h1), tH2 = getSize(h2), tH3 = getSize(h3);  
 
-  while(true){
+  while(!h1.empty() && !h2.empty() && !h3.empty()){
     if(tH1 == tH2 && tH2 == tH3){
       return tH1;
     } else if (tH1 > tH2 && tH1 > tH3){
-      h1.pop_back;
+      h1.erase(h1.begin());
     } else if (tH2 > tH1 && tH2 > tH3){
-      h2.pop_back;
+      h2.erase(h2.begin());
     } else if (tH3 > tH1 && tH3 > tH2){
-      h3.pop_back;
+      h3.erase(h3.begin());
     }
     tH1 = getSize(h1);
     tH2 = getSize(h2);
