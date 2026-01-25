f488d0144d78ebcbab49187885cd6107d84836be
Cavan Theiss <cavanrt@gmail.com>
2026-01-24 19:25:04 -0600

    Update EqualStacks.cpp

--- EqualStacks.cpp
+++ EqualStacks.cpp
@@ -1,7 +1,7 @@
 //*********************************************************************
 int getSize(const vector& cylinder){
   
-  int size = 0;
+  std::size_t size = 0;
 
   for(std::size_t i = 0; i < cylinder.size(); i++){
   
@@ -16,7 +16,7 @@ int equalStacks(vector<int> h1, vector<int> h2, vector<int> h3) {
 
   // Record the total height for each stack
   // tH1 - tH2 - tH3
-  int tH1 = getSize(h1), tH2 = getSize(h2), tH3 = getSize(h3);
+  std::size_t tH1 = getSize(h1), tH2 = getSize(h2), tH3 = getSize(h3);
   
   // Find the highest tH & remove the top cylinder from the associated stack
 
