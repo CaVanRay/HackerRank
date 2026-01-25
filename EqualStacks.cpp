1980afa6bcb5f2500dcf45a35e4a05a58019f8a2
Cavan Theiss <cavanrt@gmail.com>
2026-01-24 18:22:51 -0600

    Update EqualStacks.cpp

--- EqualStacks.cpp
+++ EqualStacks.cpp
@@ -1,1 +1,14 @@
 
+/*
+ * Complete the 'equalStacks' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY h1
+ *  2. INTEGER_ARRAY h2
+ *  3. INTEGER_ARRAY h3
+ */
+
+int equalStacks(vector<int> h1, vector<int> h2, vector<int> h3) {
+
+}
