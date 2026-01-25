9052bd3afc7f03c49246ec058afdf500cfc49cd4
Cavan Theiss <cavanrt@gmail.com>
2026-01-24 19:40:57 -0600

    Update EqualStacks.cpp

--- EqualStacks.cpp
+++ EqualStacks.cpp
@@ -14,19 +14,13 @@ int getSize(const vector& cylinder){
 
 int equalStacks(vector<int> h1, vector<int> h2, vector<int> h3) {
 
-  // Record the total height for each stack and create variable for matching height
-  // tH1, tH2, tH3
-  std::size_t tH1 = getSize(h1), tH2 = getSize(h2), tH3 = getSize(h3), mH = 0;
-  
-  // Find the highest tH & remove the top cylinder from the associated stack
-  
-  
-  // Compare tH from each stack
+  std::size_t tH1 = getSize(h1), tH2 = getSize(h2), tH3 = getSize(h3);  
 
-  
-  // if all tH are equal, return height
-  // if not matching, go back to top of loop
+  while(true){
 
-  
+    if(tH1 == tH2 && tH2 == tH3){
+      return tH1;
+    } 
+  }
 }
 //*********************************************************************
