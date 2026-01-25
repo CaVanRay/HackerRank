98e223236a6ba5f136b668a93d7483fa61665202
Cavan Theiss <cavanrt@gmail.com>
2026-01-25 15:57:38 -0600

    Update GameOfTwoStacks.cpp

--- GameOfTwoStacks.cpp
+++ GameOfTwoStacks.cpp
@@ -1,1 +1,13 @@
+/*
+ * Complete the 'twoStacks' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER maxSum
+ *  2. INTEGER_ARRAY a
+ *  3. INTEGER_ARRAY b
+ */
 
+int twoStacks(int maxSum, vector<int> a, vector<int> b) {
+
+}
