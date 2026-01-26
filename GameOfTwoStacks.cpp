fd9367234dea34982358068cf6dc5e57ce3e8241
Cavan Theiss <cavanrt@gmail.com>
2026-01-25 21:25:16 -0600

    Update GameOfTwoStacks.cpp

--- GameOfTwoStacks.cpp
+++ GameOfTwoStacks.cpp
@@ -2,13 +2,16 @@
 int twoStacks(const int& maxSum,const vector<int>& a,const vector<int>& b) {
 
   int* aP = a[0], bP = b[0];
-  std::type_t sum = 0, maxRemove = 0;
+  std::size_t sum = 0, maxRemove = 0, i = 0;
 
-  size_t i = 0;
-  while(sum <= maxSum && i < a.size()){
-  
-  sum = sum + 
-    
+  while(sum < maxSum && i < a.size()){
+    if((sum + a[i]) <= maxSum){
+      sum = sum + a[i];
+      aP = a[i];
+    }else{
+      break;
+    }
+      i++;
   }
 
 }
