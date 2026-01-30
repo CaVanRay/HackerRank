f84370060cb76a2a85b706ea6a2fe81cd8fc9813
Cavan Theiss <cavanrt@gmail.com>
2026-01-30 15:50:54 -0600

    Update GameOfTwoStacks.cpp

--- GameOfTwoStacks.cpp
+++ GameOfTwoStacks.cpp
@@ -24,7 +24,7 @@ int twoStacks(const int& maxSum,const vector<int>& a,const vector<int>& b) {
   while(i >= 0 || j < b.size()){
     
   // Step 4.) Add a B
-    
+  sum += b[j];
   // Step 5.) is there space? Yes -> Record Max -> goto step 3.) | No -> Continue
   // Step 6.) is Sum over Max? No -> Record Max -> goto step 3.) | Yes -> Continue
   // Step 7.) Does Sum contain A's? Yes -> Remove an A -> goto step 5.) | No -> Return Max Removals
