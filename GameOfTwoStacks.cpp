ff09ffab61e24730c94d2367e873846c3f81e795
Cavan Theiss <cavanrt@gmail.com>
2026-02-02 16:40:09 -0600

    Update GameOfTwoStacks.cpp

--- GameOfTwoStacks.cpp
+++ GameOfTwoStacks.cpp
@@ -18,7 +18,7 @@ int twoStacks(const int& maxSum,const vector<int>& a,const vector<int>& b) {
   //***********************************************
 
   // Second the swap with B's**********************
-  while(i >= 0 || j < b.size()){
+  while(i >= 0 && j < b.size()){
     sum += b[j];
     j++;
     while(sum > maxSum){
