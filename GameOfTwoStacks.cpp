fa294b71cb2e307903891c28580cfa2210cdf665
Cavan Theiss <cavanrt@gmail.com>
2026-02-02 16:30:50 -0600

    Update GameOfTwoStacks.cpp

--- GameOfTwoStacks.cpp
+++ GameOfTwoStacks.cpp
@@ -10,7 +10,7 @@ int twoStacks(const int& maxSum,const vector<int>& a,const vector<int>& b) {
       sum += a[i];
       aP = a[i];
       maxRemove = i + 1;
-      // Step 2.) Record maxremovals
+      
     }else{
       break;
     }
