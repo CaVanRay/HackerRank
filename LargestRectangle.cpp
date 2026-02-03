031b4fdcf408e41cfdb429c73412a723303d2f05
Cavan Theiss <cavanrt@gmail.com>
2026-02-03 09:05:56 -0600

    Update LargestRectangle.cpp

--- LargestRectangle.cpp
+++ LargestRectangle.cpp
@@ -3,10 +3,13 @@
 long largestRectangle(const vector<int>& h) {
   
   long int largestRect = 0;
+  int size, count;
 
   for(int i = 0; i < h.size(); i++){
-
-    
+  
+    size = h[i];
+    count = 0;
+  
   }
 
   return largestRect;
