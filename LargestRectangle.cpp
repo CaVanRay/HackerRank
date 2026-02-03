2884aa1fa1fdab1bb8b54b989037b298bdb8894c
Cavan Theiss <cavanrt@gmail.com>
2026-02-03 06:31:04 -0600

    Update LargestRectangle.cpp

--- LargestRectangle.cpp
+++ LargestRectangle.cpp
@@ -1,1 +1,14 @@
+//****************************************************************
 
+/*
+ * Complete the 'largestRectangle' function below.
+ *
+ * The function is expected to return a LONG_INTEGER.
+ * The function accepts INTEGER_ARRAY h as parameter.
+ */
+
+long largestRectangle(vector<int> h) {
+
+}
+
+//****************************************************************
