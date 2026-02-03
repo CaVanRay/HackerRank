629df1535daa4846bbd91efd13d76fe891374a2c
Cavan Theiss <cavanrt@gmail.com>
2026-02-03 11:29:49 -0600

    Update LargestRectangle2.cpp

--- LargestRectangle2.cpp
+++ LargestRectangle2.cpp
@@ -1,3 +1,4 @@
+// Rewriting this to use a monotonic stack
 //****************************************************************
 
 long largestRectangle(const vector<int>& h) {
@@ -7,29 +8,6 @@ long largestRectangle(const vector<int>& h) {
 
   for(int i = 0; i < size; i++){
   
-    height = h[i];
-    width = 1;
-
-    if(i > 0){ 
-      for(int j = i - 1; j >= 0; j--){
-        if(h[j] >= h[i]){
-          width++;
-        }else break;
-      } 
-    }
-
-    if(i < size - 1){
-      for(int j = i + 1; j < size; j++){
-        if(h[j] >= h[i]){
-          width++;
-        }else break;
-      }    
-    }
-
-    if((width*height) > largestRect){
-        largestRect = (width*height);
-    }
- 
   }
   
   return largestRect;
