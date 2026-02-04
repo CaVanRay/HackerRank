dfa939afc1f9285952d1615350e6d09f8d9ac054
Cavan Theiss <cavanrt@gmail.com>
2026-02-04 09:52:31 -0600

    Update LargestRectangle2.cpp

--- LargestRectangle2.cpp
+++ LargestRectangle2.cpp
@@ -4,8 +4,10 @@
 long largestRectangle(const vector<int>& h) {
   
   long int largestRect = 0;
-  int height, width, size = h.size();
-
+  int height, width, area, index, Pindex, Nindex, size = h.size();
+  vector <int> Bheight;
+  vector <int> Bindex;
+  
   for(int i = 0; i < size; i++){
   
   }
