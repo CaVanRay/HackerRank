e81970dfa561db1d52a6150c5761f9731892e6e4
Cavan Theiss <cavanrt@gmail.com>
2026-02-04 13:20:06 -0600

    Update LargestRectangle2.cpp

--- LargestRectangle2.cpp
+++ LargestRectangle2.cpp
@@ -4,44 +4,34 @@
 long largestRectangle(const vector<int>& h) {
   
   long int largerRect = 0;
-  int height, width, area, index, pIndex, nIndex, secondToLast, size = h.size();
-  vector <int> bHeight;
-  vector <int> bIndex;
+  int height, width, area, prevIndex, nextIndex, secondToLast, size = h.size();
+  vector <int> stack;
 
   // First section is for running through vector h, populating the stack,
   // and removing items when drops occurr
   
   for(int i = 0; i < size; i++){
-    while(!bHeight.empty() && bHeight.back() > h[i]){
-      height = bHeight.back();
-      index = bIndex.back();
-      nIndex = size;
-      if(bIndex.size() >= 2){
-        secondToLast = bIndex.size() - 2;
-        pIndex = bIndex[secondToLast];
-      }else{
-        pIndex = -1;
-      }
-      width = nIndex - pIndex - 1;
+    while(!stack.empty() && h[stack.back()] > h[i]){
+      height = h[stack.back()];
+      nextIndex = stack.size();
+      prevIndex = stack.empty() ? -1 : stack.back()
+      width = nextIndex - prevIndex - 1;
       area = width*height;
       if(area > largerRect){
         largerRect = area;
       }
-      bHeight.pop_back();
-      bIndex.pop_back();
+      stack.pop_back();
     }
-    bHeight.push_back(h[i]);
-    bIndex.push_back(i+1);
+    stack.push_back(i);
   }
 
   // Second section is for cleaning up the items still in the stack
 
   while(!bHeight.empty()){
     height = bHeight.back();
-    index = bIndex.back();
-    nIndex = i;
-    if(bIndex.size() >= 2){
-      secondToLast = bIndex.size() - 2;
+    nIndex = size;
+    if(stack.size() >= 2){
+      secondToLast = stack.size() - 2;
       pIndex = bIndex[secondToLast];
     }else{
       pIndex = -1;
@@ -52,7 +42,7 @@ long largestRectangle(const vector<int>& h) {
       largerRect = area;
     }
     bHeight.pop_back();
-    bIndex.pop_back();
+    stack.pop_back();
   }
   
   return largerRect;
