e0fcc75076f45a7c6a7c059ad1a35e85800f101d
Cavan Theiss <cavanrt@gmail.com>
2026-02-05 15:41:27 -0600

    Update PoisonousPlants.cpp

--- PoisonousPlants.cpp
+++ PoisonousPlants.cpp
@@ -4,11 +4,12 @@ int poisonousPlants(vector<int> plants) {
   int daysToStasis, numberOfPlants = plants.size();
   vector<int>stack;
 
-  for(int i = plants.size() - 1; i > 0; i--){
+  for(int i = plants.size() - 1; i >= 0; i--){
     
     while(!stack.empty() && plants[i] > stack.back()){
       
     }
+    
     
   }
   
