30409dc895fe51a065d76b54ac8cbe6ae154d5a7
Cavan Theiss <cavanrt@gmail.com>
2026-02-05 15:33:37 -0600

    Update PoisonousPlants.cpp

--- PoisonousPlants.cpp
+++ PoisonousPlants.cpp
@@ -1,9 +1,4 @@
-/*
- * Complete the 'poisonousPlants' function below.
- *
- * The function is expected to return an INTEGER.
- * The function accepts INTEGER_ARRAY p as parameter.
- */
+//**********************************************************
 
 int poisonousPlants(vector<int> plants) {
   int daysToStasis, numberOfPlants = plants.size();
