20a7c82a419ea8593788703ae9b8011a6e0d8194
Cavan Theiss <cavanrt@gmail.com>
2026-02-05 11:10:13 -0600

    Update SimpleTextEditor.cpp

--- SimpleTextEditor.cpp
+++ SimpleTextEditor.cpp
@@ -5,8 +5,66 @@
 #include <algorithm>
 using namespace std;
 
+void append(std::string current){
+    std::string addThis;
+    std::cin >> addThis;
+    current.append(addThis);
+}
 
+void delete(std::string current){
+    int toRemove;
+    std::cin >> toRemove;
+    current.erase(current.length() - toRemove, toRemove);
+}
+
+void print(std::string current){
+    int toPrint;
+    std::cin >> toPrint;
+    char letterToPrint = current[toPrint];
+    std::cout << letterToPrint << std::endl;
+}
+
+void undo(std::string current, vector<string>pastStates){
+    current = pastStates.back();
+    pastStates.pop_back();
+}
+    
 int main() {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    
+    std::string current;
+    std::vector<std::string> pastStates;
+    int numberOfActions, thisAction;
+
+    std::cin >> numberOfActions; 
+
+    for<int i = 0; i < numberOfActions; i++){
+
+        std::cin >> thisAction;
+
+        switch(thisAction){
+            case 1 :
+            pastStates.push_back(current);
+            append(current);
+            break;
+            
+            case 2 :
+            pastStates.push_back(current);
+            delete(current);
+            break;
+
+            case 3 :
+            print(current);
+            break;
+
+            case 4 :
+            undo(current, pastStates);
+            break;
+
+            default :
+            std::cout << "Error: Invalid Action";
+            
+        }
+    }
+    
     return 0;
 }
