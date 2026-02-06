a153828bee9d03664edb159fa0e7daa004eaeb84
Cavan Theiss <cavanrt@gmail.com>
2026-02-06 16:15:06 -0600

    Update PoisonousPlants.cpp

--- PoisonousPlants.cpp
+++ PoisonousPlants.cpp
@@ -4,8 +4,7 @@ int poisonousPlants(vector<int> plants) {
   int daysToStasis = 0, numberOfPlants = plants.size();
   vector<int>stack;
 
-  // I need to only allow 1 pop per match-up
-  // then track the number of rounds it takes
+  // re-doing this from scratch, I need to account for temporal drift
 
   
   return daysToStasis;
