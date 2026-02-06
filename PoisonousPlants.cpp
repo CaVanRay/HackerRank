494252e56c3e8da45fdccb32ea44991bdbe527df
Cavan Theiss <cavanrt@gmail.com>
2026-02-06 15:38:47 -0600

    Update PoisonousPlants.cpp

--- PoisonousPlants.cpp
+++ PoisonousPlants.cpp
@@ -5,6 +5,7 @@ int poisonousPlants(vector<int> plants) {
   vector<int>stack;
 
   // I need to only allow 1 pop per match-up
+  // then track the number of rounds it takes
   
   
   for(int i = plants.size() - 1; i >= 0; i--){
