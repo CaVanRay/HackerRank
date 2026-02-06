bcd4de230d7fb0428647777ee61162a216c8b812
Cavan Theiss <cavanrt@gmail.com>
2026-02-06 15:07:56 -0600

    Update PoisonousPlants.cpp

--- PoisonousPlants.cpp
+++ PoisonousPlants.cpp
@@ -1,7 +1,7 @@
 //**********************************************************
 
 int poisonousPlants(vector<int> plants) {
-  int popsInARow = 0, daysToStasis, numberOfPlants = plants.size();
+  int popsInARow = 0, daysToStasis = 0, numberOfPlants = plants.size();
   vector<int>stack;
 
   for(int i = plants.size() - 1; i >= 0; i--){
@@ -10,6 +10,7 @@ int poisonousPlants(vector<int> plants) {
       // pop item at stack.back()
       // daysToStasis will equal maximum number of succsesive pops in a row
       // when pops stop and a push occurrs, the count starts over
+      
     }
     stack.push_back(i);
     popsInARow = 0;
