b2ffced129df154a7d93f0549374e3cf0e86daac
Cavan Theiss <cavanrt@gmail.com>
2026-02-08 19:14:41 -0600

    Add header comments for PoisonousPlants functionality
    Add header comments to PoisonousPlants.cpp

--- PoisonousPlants.cpp
+++ PoisonousPlants.cpp
@@ -4,7 +4,7 @@ Title: Poisonous Plants
 Author: Cavan Theiss
 Description: Calculates the maximum number of days until no more plants will die
 Rules: any plant that has more pesticide than the plant to their left will die
-
+Input: Vector of integers, these are the pesticide levels for each plant
 */
 
 int poisonousPlants(vector<int> plants) {
