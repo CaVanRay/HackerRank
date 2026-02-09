bc5f0e06c18aca68e1366fd616dd27e681a8bc6c
Cavan Theiss <cavanrt@gmail.com>
2026-02-08 19:14:08 -0600

    Add comments to PoisonousPlants.cpp
    Added header comments with title, author, and description.

--- PoisonousPlants.cpp
+++ PoisonousPlants.cpp
@@ -3,6 +3,7 @@
 Title: Poisonous Plants
 Author: Cavan Theiss
 Description: Calculates the maximum number of days until no more plants will die
+Rules: any plant that has more pesticide than the plant to their left will die
 
 */
 
