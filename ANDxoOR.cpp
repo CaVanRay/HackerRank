f2e05f37fee8dbe42772bd36b933fc994828c69f
Cavan Theiss <cavanrt@gmail.com>
2026-02-09 19:07:04 -0600

    Update ANDxoOR.cpp

--- ANDxoOR.cpp
+++ ANDxoOR.cpp
@@ -1,1 +1,11 @@
 
+/*
+ * Complete the 'andXorOr' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY a as parameter.
+ */
+
+int andXorOr(vector<int> a) {
+
+}
