60ff8fa4a56e670d4784e3048ed81573b6f2541e
Cavan Theiss <cavanrt@gmail.com>
2026-02-09 19:15:34 -0600

    Update ANDxoOR.cpp

--- ANDxoOR.cpp
+++ ANDxoOR.cpp
@@ -8,11 +8,6 @@ Input: vector of integers
 Output: returns an integer
 */
 
-/*
- * The function is expected to return an INTEGER.
- * The function accepts INTEGER_ARRAY a as parameter.
- */
-
 int andXorOr(vector<int> a) {
 
 }
