0dabce31855ad913091aa28b3f8dd478a2f5edb0
Cavan Theiss <cavanrt@gmail.com>
2026-02-10 14:54:07 -0600

    Update waiter.cpp

--- waiter.cpp
+++ waiter.cpp
@@ -12,14 +12,19 @@ Output: vector of integers
 
 vector<int> waiter(vector<int> number, int q) {
 
-vector<int> pileA, pileB;
+vector<int> answer, pending;
  
 /*
 q is the number of iterations we have to go through
 each iteration is based on a prime number starting 
 at 2 going up
 
+the first iteration goes through the provided vector
+and any items divisible by that iteration's prime number
+are added to the answer vector while the rest are added 
+to the pending vector
 
+each subsequent iterations
 */
  
 }
