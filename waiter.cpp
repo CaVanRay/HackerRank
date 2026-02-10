821a8ee7a67c5817c4e96038e87ebfbe37b075fb
Cavan Theiss <cavanrt@gmail.com>
2026-02-10 14:51:17 -0600

    Update waiter.cpp

--- waiter.cpp
+++ waiter.cpp
@@ -15,6 +15,10 @@ vector<int> waiter(vector<int> number, int q) {
 vector<int> pileA, pileB;
  
 /*
+q is the number of iterations we have to go through
+each iteration is based on a prime number starting 
+at 2 going up
+
 
 */
  
