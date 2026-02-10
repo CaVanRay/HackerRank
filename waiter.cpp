ee84851f6aad4b3bc71926cd56fd39bfb0f33a0c
Cavan Theiss <cavanrt@gmail.com>
2026-02-10 11:34:26 -0600

    Update waiter.cpp

--- waiter.cpp
+++ waiter.cpp
@@ -1,1 +1,12 @@
+/*
+ * Complete the 'waiter' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY number
+ *  2. INTEGER q
+ */
 
+vector<int> waiter(vector<int> number, int q) {
+
+}
