f7f550be59145ff935a4b339f8e5a882fe217317
Cavan Theiss <cavanrt@gmail.com>
2026-02-11 09:34:49 -0600

    Update waiter.cpp

--- waiter.cpp
+++ waiter.cpp
@@ -18,7 +18,9 @@ vector<int> answer, pending;
 q is the number of iterations we have to go through
 each iteration is based on a prime number starting 
 at 2 going up
-
+*/
+ 
+/*
 the first iteration goes through the provided vector
 and any items divisible by that iteration's prime number
 are added to the answer vector while the rest are added 
