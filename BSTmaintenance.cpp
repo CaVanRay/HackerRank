6b8a6f4cae1e8318e0dd0eb42f4abb481eade9cd
Cavan Theiss <cavanrt@gmail.com>
2026-02-12 10:32:32 -0600

    Update BSTmaintenance.cpp

--- BSTmaintenance.cpp
+++ BSTmaintenance.cpp
@@ -1,10 +1,7 @@
 //***********************************************************************************
 
 /*
- * Complete the 'solve' function below.
- *
- * The function is expected to return an INTEGER_ARRAY.
- * The function accepts INTEGER_ARRAY arr as parameter.
+Title: BST maintenance
  */
 
 vector<int> solve(vector<int> arr) {
