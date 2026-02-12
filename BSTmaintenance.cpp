cb2d4e49f96d8b8213441f2d5cb3932535f51d0a
Cavan Theiss <cavanrt@gmail.com>
2026-02-12 10:37:36 -0600

    Update BSTmaintenance.cpp

--- BSTmaintenance.cpp
+++ BSTmaintenance.cpp
@@ -5,7 +5,9 @@ Title: BST maintenance
 Author: Cavan Ray Theiss
 Description: given a series of numbers build out a binary tree and once done
 return the total size of all distances between every node on the tree
-Input: Vector of integers
+Input: Vector of integers that will be used to build the binary tree
+Output: Vector of integers that show the total distances between nodes after each
+item is added to the binary tree
  */
 
 vector<int> solve(vector<int> arr) {
