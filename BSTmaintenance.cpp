fb0f29755d0773afad0d8cbf0864e486794ada09
Cavan Theiss <cavanrt@gmail.com>
2026-02-16 20:08:38 -0600

    Update BSTmaintenance.cpp

--- BSTmaintenance.cpp
+++ BSTmaintenance.cpp
@@ -19,8 +19,28 @@ in a simple 2 leg tree the total length increases at a regular predictable rate
 
 total starts at 0
 
-each succesive addition increases by 1
+each succesive addition increases by 1 while the total rolls over
 
+total = 0
+round 1: addition = + 0,                       total = total + 0                       = 0
+round 2: addition = + 1,                       total = total + 1                       = 1
+round 3: addition = + 1 + 2,                   total = total + 1 + 2                   = 4
+round 4: addition = + 1 + 2 + 3                total = total + 1 + 2 + 3               = 10
+round 5: addition = + 1 + 2 + 3 + 4            total = total + 1 + 2 + 3 + 4           = 20
+round 6: addition = + 1 + 2 + 3 + 4 + 5        total = total + 1 + 2 + 3 + 4 + 5       = 35
+
+for a simple 2 leg loop this is great, but I need to find a logic path for how additional legs effect the increase
+
+**************EXAMPLE***************
+
+int  addLoop(int start){
+  int total = 0
+  for(int i; i < start; i++){
+    total += i;
+  }
+}
+
+************************************
 
 */
   
