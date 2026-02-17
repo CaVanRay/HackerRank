75255fd07302a960365989f0521791147efbbaff
Cavan Theiss <cavanrt@gmail.com>
2026-02-16 19:48:21 -0600

    Update BSTmaintenance.cpp

--- BSTmaintenance.cpp
+++ BSTmaintenance.cpp
@@ -13,75 +13,7 @@ item is added to the binary tree
 vector<int> solve(vector<int> arr) {
 
 /* 
-asking for a walkthrough of the logic needed for this problem gave chatgpt an aneurysm 
-
-I'm going to pen & paper for the rest of the day, work on the logic in my freetime & 
-hopefully have an idea of how I'm gonna do this tomorrow morning
-
-I think this is a monotonic stack problem, but I still can't work out the logic
-
-I'm gonna try to figure it out tonight after work, but i might also just go to sleep
-worst case scenario I'll work on it tomorrow
-*/  
-
-/*
-Example: 
-input  - 4, 7, 3,  1,  8,  2,  6,  5
-output - 0, 1, 4, 10, 20, 35, 52, 76
-*/
-
-/*
-input = 4
-depth = 0
-branches = 1
-total = 0
-*/
-
-/*
-input = 7
-depth = 1
-branches = 1
-total = 0 + 1 = 1
-*/
-
-/*
-input = 3
-depth = 1
-branches = 2
-total = 1 + 3 = 4
-*/
-
-/*
-input = 1
-depth = 2
-branches = 2
-total = 4 + 6 = 10
-*/
-
-/*
-input = 8
-depth = 2
-branches = 2
-total = 10 + 10 = 20
-*/
-
-/*
-input = 2
-depth = 3
-branches = 2
-total = 20 + 15 = 35
-*/
-
-/*
-input = 6
-depth = 2
-branches = 3
-total = 35 + 17 = 52
-*/
-
-/*
-input = 5
-depth = 3
+I think I found finger-hold
 */
   
 }
