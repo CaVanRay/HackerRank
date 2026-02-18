b5a186cc29440833b63f873de4ccee90d4eb85fe
Cavan Theiss <cavanrt@gmail.com>
2026-02-18 09:00:53 -0600

    Update AVeryBigSum.cpp

--- AVeryBigSum.cpp
+++ AVeryBigSum.cpp
@@ -3,6 +3,8 @@
 /*
  Title: A Very Big Sum
  Author: Cavan Ray Theiss
+ Description: given an array of very large numbers, add them 
+ all together & return the total sum
  * The function is expected to return a LONG_INTEGER.
  * The function accepts LONG_INTEGER_ARRAY ar as parameter.
  */
