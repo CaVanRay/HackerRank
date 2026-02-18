4483c74374c4f4ed579612a143f6421c87966797
Cavan Theiss <cavanrt@gmail.com>
2026-02-17 20:09:01 -0600

    Update CompareTheTriplets.cpp

--- CompareTheTriplets.cpp
+++ CompareTheTriplets.cpp
@@ -9,15 +9,6 @@ Input: 2 integer arrays or 3 items each
 Output: 1 integer array of 2 items
 */
 
-/*
- * Complete the 'compareTriplets' function below.
- *
- * The function is expected to return an INTEGER_ARRAY.
- * The function accepts following parameters:
- *  1. INTEGER_ARRAY a
- *  2. INTEGER_ARRAY b
- */
-
 vector<int> compareTriplets(vector<int> a, vector<int> b) {
 
 }
