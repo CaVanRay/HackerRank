eca45517e6331ae6786f25364d6dccb0ba2ae13b
Cavan Theiss <cavanrt@gmail.com>
2026-02-18 09:15:56 -0600

    Update DiagonalDifference.cpp

--- DiagonalDifference.cpp
+++ DiagonalDifference.cpp
@@ -3,6 +3,9 @@
 /*
 Title: Diagonal Difference
 Author: Cavan Ray Theiss
+Description: given a 3X3 array of numbers calculate the absolute
+difference of sums for the 2 diagonals
+
 */
 
 int diagonalDifference(vector<vector<int>> arr) {
