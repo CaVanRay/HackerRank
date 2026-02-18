7cae059cee9f35bb8e506ad011f1e5529ca6c70d
Cavan Theiss <cavanrt@gmail.com>
2026-02-18 10:25:18 -0600

    Update PlusMinus.cpp

--- PlusMinus.cpp
+++ PlusMinus.cpp
@@ -13,7 +13,22 @@ Output: print the 3 ratios to the screen in decimal value to 6 places
 */
 
 void plusMinus(vector<int> arr) {
+float positives = 0, negatives = 0, zeros = 0;
+  for(int i = 0; i < arr.size(); i++){
+    if(arr[i] > 0){
+      positives++;
+    }else if(arr[i] < 0){
+      negatives++;
+    }else{
+      zeros++
+    }
+  }
 
+  cout << fixed << setprecision(6) << (positives/arr.size()) << endl;
+  cout << (negatives/arr.size()) << endl;
+  cout << (zeros/arr.size()) << endl;
+  
+  
 }
 
 //************************************************************
