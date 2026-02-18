b8fcf92f4e839ceaa72de2f4a80042a2ae4aa605
Cavan Theiss <cavanrt@gmail.com>
2026-02-18 09:01:56 -0600

    Update AVeryBigSum.cpp

--- AVeryBigSum.cpp
+++ AVeryBigSum.cpp
@@ -5,8 +5,8 @@
  Author: Cavan Ray Theiss
  Description: given an array of very large numbers, add them 
  all together & return the total sum
- Input: and array of type Long integers
- 
+ Input: an array of type Long integers
+ Output: a single type long integer
  */
 
 long aVeryBigSum(vector<long> ar) {
