bb87fda091504373d48520319d5827c2df15ff55
Cavan Theiss <cavanrt@gmail.com>
2026-02-17 20:08:51 -0600

    Update CompareTheTriplets.cpp

--- CompareTheTriplets.cpp
+++ CompareTheTriplets.cpp
@@ -6,7 +6,7 @@ Author: Cavan Ray Theiss
 Description: Compare the scores of 2 people for 3 challenges, return the number 
 of wins for each person - (#, #)
 Input: 2 integer arrays or 3 items each
-Output: 1 integer array
+Output: 1 integer array of 2 items
 */
 
 /*
