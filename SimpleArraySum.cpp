5aab00b31acdd171ea424c03d3c480fc6644c5f9
Cavan Theiss <cavanrt@gmail.com>
2026-02-17 19:58:01 -0600

    Update SimpleArraySum.cpp

--- SimpleArraySum.cpp
+++ SimpleArraySum.cpp
@@ -11,7 +11,7 @@ the total sum of all of the integers
 
 int simpleArraySum(vector<int> ar) {
   int totalSum = 0;
-  for(i = 0; i < ar.size(); i++){
+  for(int i = 0; i < ar.size(); i++){
     totalSum += ar[i];
   }
   return totalSum;
