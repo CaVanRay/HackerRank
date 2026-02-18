5937120e88deea1d142387c61a0ebd2061a616b1
Cavan Theiss <cavanrt@gmail.com>
2026-02-18 09:13:17 -0600

    Update DiagonalDifference.cpp

--- DiagonalDifference.cpp
+++ DiagonalDifference.cpp
@@ -1,1 +1,10 @@
+/*
+ * Complete the 'diagonalDifference' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+ */
 
+int diagonalDifference(vector<vector<int>> arr) {
+
+}
