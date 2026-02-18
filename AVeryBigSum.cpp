18cbb6a43229028e57e06115d421b5d49686e232
Cavan Theiss <cavanrt@gmail.com>
2026-02-18 08:56:47 -0600

    Update AVeryBigSum.cpp

--- AVeryBigSum.cpp
+++ AVeryBigSum.cpp
@@ -1,1 +1,11 @@
 
+/*
+ * Complete the 'aVeryBigSum' function below.
+ *
+ * The function is expected to return a LONG_INTEGER.
+ * The function accepts LONG_INTEGER_ARRAY ar as parameter.
+ */
+
+long aVeryBigSum(vector<long> ar) {
+
+}
