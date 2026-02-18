04fafae024d7bf254401b0cdce40ae1957471dae
Cavan Theiss <cavanrt@gmail.com>
2026-02-18 10:17:18 -0600

    Update PlusMinus.cpp

--- PlusMinus.cpp
+++ PlusMinus.cpp
@@ -1,8 +1,19 @@
+//************************************************************
+
 /*
 Titla: Plus Minus 
 Author: Cavan Ray Theiss
+
+Description: Given an array of integers, calculate the ratios of 
+its elements that are Positive, Negative, and Zero. Print the 
+decimal value of each fraction on a new line with 6 places after the decimal.
+
+Input: an array of integers
+Output: print the 3 ratios to the screen in decimal value to 6 places
 */
 
 void plusMinus(vector<int> arr) {
 
 }
+
+//************************************************************
