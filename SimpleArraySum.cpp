8809ee65bfac8060ee27e2737dc48b34d6595076
Cavan Theiss <cavanrt@gmail.com>
2026-02-17 19:56:26 -0600

    Update SimpleArraySum.cpp

--- SimpleArraySum.cpp
+++ SimpleArraySum.cpp
@@ -1,10 +1,12 @@
 //***************************************************************
 
 /*
+
 Title: Simple Array Sum
 Author: Cavan Ray Theiss
 Description: it takes in an array of integers and then outputs
 the total sum of all of the integers
+
  */
 
 int simpleArraySum(vector<int> ar) {
