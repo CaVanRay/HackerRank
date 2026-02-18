26f94d8143e485ab5e43e974a69c238f37736c23
Cavan Theiss <cavanrt@gmail.com>
2026-02-17 19:05:03 -0600

    Update SimpleArraySum.cpp

--- SimpleArraySum.cpp
+++ SimpleArraySum.cpp
@@ -1,1 +1,10 @@
+/*
+ * Complete the 'simpleArraySum' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY ar as parameter.
+ */
 
+int simpleArraySum(vector<int> ar) {
+
+}
