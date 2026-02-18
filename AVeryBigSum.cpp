e621bb503e998a1ed254207a46ccb5ad706df492
Cavan Theiss <cavanrt@gmail.com>
2026-02-18 09:03:37 -0600

    Update AVeryBigSum.cpp

--- AVeryBigSum.cpp
+++ AVeryBigSum.cpp
@@ -10,7 +10,11 @@
  */
 
 long aVeryBigSum(vector<long> ar) {
-
+long int sum = 0;
+ for(int i = 0; i < ar.size(); i++){
+  sum += ar[i];
+ }
+ return sum;
 }
 
 //****************************************************************
