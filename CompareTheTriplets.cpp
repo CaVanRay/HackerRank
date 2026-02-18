b6c572f9cb7c268dc2d620c7952b26ab87ed696f
Cavan Theiss <cavanrt@gmail.com>
2026-02-17 20:17:27 -0600

    Update CompareTheTriplets.cpp

--- CompareTheTriplets.cpp
+++ CompareTheTriplets.cpp
@@ -10,10 +10,15 @@ Output: 1 integer array of 2 items
 */
 
 vector<int> compareTriplets(vector<int> a, vector<int> b) {
-vector<int> scores (0, 0);
-  for
-
-  
+  vector<int> scores (0, 0);
+    for(int i = 0; i < 3; i++){
+      if(a[i] > b[i]){
+        scores[0]++;
+      }else if(a[i] < b[i]){
+        scores[1]++;
+      }
+    }
+  return scores;
 }
 
 //**********************************************************************
