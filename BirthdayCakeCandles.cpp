df056763a1ddfd8c0e030eb5e51eb18dfd5ceb9a
Cavan Theiss <cavanrt@gmail.com>
2026-02-19 15:59:40 -0600

    Update BirthdayCakeCandles.cpp

--- BirthdayCakeCandles.cpp
+++ BirthdayCakeCandles.cpp
@@ -3,9 +3,11 @@
 /*
 Title: Birthday Cake Candles
 Author: Cavan Ray Theiss
-Description:
+Description: only the tallest of the candles will be blown out, we are 
+given an array of candle heights and using that we will return a count
+of how many candles will be blown out
 Input: integer array
-
+Output: integer
 */
 
 
