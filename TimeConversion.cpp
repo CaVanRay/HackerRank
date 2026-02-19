b1d71779f6ee83f315051cd452709f372544c315
Cavan Theiss <cavanrt@gmail.com>
2026-02-19 16:18:46 -0600

    Create TimeConversion.cpp

---
+++ TimeConversion.cpp
@@ -0,0 +1,17 @@
+//**********************************************************
+
+/*
+Title: Time Conversion
+Author: Cavan Ray Theiss
+Description: Take a string input of a civilian time and 
+convert it to military time before outputting it in the
+same format - HH:MM:SS:(AM/PM)
+Input: a single string
+Output: a single string
+*/
+
+string timeConversion(string s) {
+
+}
+
+//**********************************************************
