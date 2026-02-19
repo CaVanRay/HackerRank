eaee44ec3492246b1fd2f0b0a7b5324a76edf937
Cavan Theiss <cavanrt@gmail.com>
2026-02-19 15:41:25 -0600

    Update Mini-Max.cpp

--- Mini-Max.cpp
+++ Mini-Max.cpp
@@ -1,10 +1,8 @@
 //****************************************************************
 
 /*
- * Complete the 'miniMaxSum' function below.
- *
- * The function accepts INTEGER_ARRAY arr as parameter.
- */
+Title: Mini-Max
+*/
 
 void miniMaxSum(vector<int> arr) {
 
