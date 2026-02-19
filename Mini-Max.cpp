df8e46fb139d56baa028dec5773583f42ba80459
Cavan Theiss <cavanrt@gmail.com>
2026-02-19 15:43:38 -0600

    Update Mini-Max.cpp

--- Mini-Max.cpp
+++ Mini-Max.cpp
@@ -8,7 +8,7 @@ Description: we are given 5 integers, we need to calculate what is
 the smallest & the largest sum we can get using 4 of the 5 integers
 and then print those sums on the screen
 
-
+Input: an array of 5 integers
 
 */
 
