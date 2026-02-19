35659e5426285d84566088524fecd426e5f01af0
Cavan Theiss <cavanrt@gmail.com>
2026-02-19 15:43:18 -0600

    Update Mini-Max.cpp

--- Mini-Max.cpp
+++ Mini-Max.cpp
@@ -3,6 +3,13 @@
 /*
 Title: Mini-Max
 Author: Cavan Ray Theiss
+
+Description: we are given 5 integers, we need to calculate what is 
+the smallest & the largest sum we can get using 4 of the 5 integers
+and then print those sums on the screen
+
+
+
 */
 
 void miniMaxSum(vector<int> arr) {
