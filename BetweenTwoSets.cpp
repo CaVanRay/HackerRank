9475fa483480705024184023103b7758c8e42429
Cavan Theiss <cavanrt@gmail.com>
2026-02-21 18:32:13 -0600

    Update BetweenTwoSets.cpp

--- BetweenTwoSets.cpp
+++ BetweenTwoSets.cpp
@@ -15,6 +15,7 @@ Output: we return a single integer
 int getTotalX(vector<int> a, vector<int> b) {
 
 vector<int> inBetweens;
+vector<int> tempInBetweens;
 
 for(int i = 0; a[0] * i <= b[0]; i++){
   inBetweens.push_back(a[0] * i);  
