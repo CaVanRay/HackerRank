575e9198d207f2ee0384ce0713499843346d4e2a
Cavan Theiss <cavanrt@gmail.com>
2026-02-21 19:02:31 -0600

    Update BetweenTwoSets.cpp

--- BetweenTwoSets.cpp
+++ BetweenTwoSets.cpp
@@ -17,11 +17,18 @@ int getTotalX(vector<int> a, vector<int> b) {
 vector<int> inBetweens;
 vector<int> tempInBetweens;
 
+  /*
+  First we use a[0] to setup a baseline list
+  */
   
   for(int i = 0; a[0] * i <= b[0]; i++){
   inBetweens.push_back(a[0] * i);  
   }
 
+  /*
+  
+  */
+  
   for(factor : a){
     for(canidate : inBetweens){
       if(canidate % factor == 0)
