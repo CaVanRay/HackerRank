bddf0908de1b1897236bc1f71ca00dde68f9ccb6
Cavan Theiss <cavanrt@gmail.com>
2026-02-22 19:09:21 -0600

    Update BreakingTheRecords.cpp

--- BreakingTheRecords.cpp
+++ BreakingTheRecords.cpp
@@ -3,7 +3,13 @@
 /*
 Title: Breaking The Records
 Author: Cavan Ray Theiss
-Description:
+
+Description: given an array of scores for games played for the whole season
+we will track the Highest & Lowest score starting with the first game and then
+track how often those records are broken, once we are done working our way
+through the list we will return a vector array containing how often the records
+for Highest & Lowest scores were broken
+
 Input: A vector array of integers
 */
 
