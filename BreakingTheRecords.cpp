487c8e370a741fcc63fe57d2239395e9f80fbd7f
Cavan Theiss <cavanrt@gmail.com>
2026-02-22 18:59:13 -0600

    Update BreakingTheRecords.cpp

--- BreakingTheRecords.cpp
+++ BreakingTheRecords.cpp
@@ -1,11 +1,8 @@
 //**************************************************************************************
 
 /*
- * Complete the 'breakingRecords' function below.
- *
- * The function is expected to return an INTEGER_ARRAY.
- * The function accepts INTEGER_ARRAY scores as parameter.
- */
+Title: Breaking The Records
+*/
 
 vector<int> breakingRecords(vector<int> scores) {
 
