f2e867712adcc44a522236256161277f2e2502ec
Cavan Theiss <cavanrt@gmail.com>
2026-02-22 19:11:46 -0600

    Update BreakingTheRecords.cpp

--- BreakingTheRecords.cpp
+++ BreakingTheRecords.cpp
@@ -10,7 +10,7 @@ track how often those records are broken, once we are done working our way
 through the list we will return a vector array containing how often the records
 for Highest & Lowest scores were broken
 
-Input: A vector array of integers
+Input: A vector array of integers representing scores
 */
 
 vector<int> breakingRecords(vector<int> scores) {
