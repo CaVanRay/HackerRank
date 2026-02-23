2464e1b65c05be11c3ea235c808b1581e789687c
Cavan Theiss <cavanrt@gmail.com>
2026-02-22 19:46:43 -0600

    Update BreakingTheRecords.cpp

--- BreakingTheRecords.cpp
+++ BreakingTheRecords.cpp
@@ -21,7 +21,7 @@ vector<int> breakingRecords(vector<int> scores) {
   for(int currentScore : scores){
     
   } 
-  
+  return recordsBroken;
 }
 
 //**************************************************************************************
