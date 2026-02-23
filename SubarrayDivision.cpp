53412437551f2b9b3a87121f47044284b2806199
Cavan Theiss <cavanrt@gmail.com>
2026-02-23 11:39:02 -0600

    Create SubarrayDivision.cpp

---
+++ SubarrayDivision.cpp
@@ -0,0 +1,23 @@
+//**********************************************************************
+
+/*
+ Title: Subarray Division
+ Author: Cavan Ray Theiss
+ 
+ Description: within the provided integer array, we need to find a 
+ contiguous section that has a length that matches the birth Month
+ and a total item sum that equals the birth Day
+
+ Input: an integer array showing the numbers assigned to each section of 
+ chocolate bar & two seperate integers representing the month & day of 
+ the given birthday
+ 
+ Output: a single integer representing the count of ways we can possibly
+ divide up the chocolate bar that fits the presented requirements 
+*/
+
+int birthday(vector<int> s, int d, int m) {
+
+}
+
+//**********************************************************************
