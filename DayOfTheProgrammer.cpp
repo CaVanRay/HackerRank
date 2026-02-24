f8c6f3f2358fc77045665a209380a1631ba9bdf5
Cavan Theiss <cavanrt@gmail.com>
2026-02-24 16:29:51 -0600

    Create DayOfTheProgrammer.cpp

---
+++ DayOfTheProgrammer.cpp
@@ -0,0 +1,17 @@
+//***************************************************************************
+
+/*
+Title: Day Of The Programmer
+Author: Cavan Ray Theiss
+Description: given the year return what day is the 256th day in the year.
+take into account both the julian & gregorian calenders
+
+Input: a single integer
+Output: a single string
+*/
+
+string dayOfProgrammer(int year) {
+
+}
+
+//***************************************************************************
