3b9388c0d495567cb5c8df0ec4b80a650a11fda3
Cavan Theiss <cavanrt@gmail.com>
2026-02-26 11:18:53 -0600

    Update BillDivision.cpp

--- BillDivision.cpp
+++ BillDivision.cpp
@@ -1,1 +1,12 @@
+/*
+ * Complete the 'bonAppetit' function below.
+ *
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY bill
+ *  2. INTEGER k
+ *  3. INTEGER b
+ */
 
+void bonAppetit(vector<int> bill, int k, int b) {
+
+}
