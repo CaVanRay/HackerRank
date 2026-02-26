8386d466e8ee269c2b9be45358a4f03f315dfbf3
Cavan Theiss <cavanrt@gmail.com>
2026-02-26 12:37:53 -0600

    Update BillDivision.cpp

--- BillDivision.cpp
+++ BillDivision.cpp
@@ -5,8 +5,9 @@ Title: Bill Division
 Author: Cavan Ray Theiss
 Description:
 
-Input: an array of integers, and 2 individual integers
-Output:
+Input: an array of integers representing the cost of each item ordered, and 2 individual integers
+- k: the item from the bill not eaten, & - b: the amount paid
+Output: either print the amount to refund or if correct amount paid print "Bon Appetit"
  */
 
 void bonAppetit(vector<int> bill, int k, int b) {
