8b88df455265894ac67d83dbd0579b9f542c95af
Cavan Theiss <cavanrt@gmail.com>
2026-02-27 16:23:44 -0600

    Update SalesByMatch.cpp

--- SalesByMatch.cpp
+++ SalesByMatch.cpp
@@ -1,1 +1,12 @@
+/*
+ * Complete the 'sockMerchant' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER_ARRAY ar
+ */
 
+int sockMerchant(int n, vector<int> ar) {
+
+}
