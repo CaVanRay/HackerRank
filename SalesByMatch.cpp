27666601cb1f0879b08616f0f47ca4b1b315ffc4
Cavan Theiss <cavanrt@gmail.com>
2026-02-27 16:24:21 -0600

    Update SalesByMatch.cpp

--- SalesByMatch.cpp
+++ SalesByMatch.cpp
@@ -1,8 +1,8 @@
 //******************************************************************************
 
 /*
- * Complete the 'sockMerchant' function below.
- *
+ Title: Sales By Match
+ 
  * The function is expected to return an INTEGER.
  * The function accepts following parameters:
  *  1. INTEGER n
