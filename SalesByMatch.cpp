bf824ca2835313535195c35fb009ff8a25045e34
Cavan Theiss <cavanrt@gmail.com>
2026-02-27 18:33:22 -0600

    Update SalesByMatch.cpp

--- SalesByMatch.cpp
+++ SalesByMatch.cpp
@@ -7,7 +7,7 @@
  Description: Given an array of integers representing the color of each sock, 
  determine how many pairs of socks with matching colors there are.
 
- Input: a single integer n, and an array of integers
+ Input: a single integer n representing how many socks are in the pile, and an array of integers
  Output: a single integer
 */
 
