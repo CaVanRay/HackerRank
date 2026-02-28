c024bc47a882d4530c7dc1630f274c311f7ce9f1
Cavan Theiss <cavanrt@gmail.com>
2026-02-28 16:44:01 -0600

    Update DrawingBook.cpp

--- DrawingBook.cpp
+++ DrawingBook.cpp
@@ -4,7 +4,8 @@
 Title: Drawing Book
 Author: Cavan Ray Theiss
 
-Description:
+Description: we need to find and print the minimum number of pages that
+must be turned in order to arrive at requested page
 
 Input: 2 individual integers
 Output: 1 individual integer
