fa79d98347c1ec278de5c4785a7b9fcb7cb019a6
Cavan Theiss <cavanrt@gmail.com>
2026-02-27 18:32:36 -0600

    Update SalesByMatch.cpp

--- SalesByMatch.cpp
+++ SalesByMatch.cpp
@@ -4,7 +4,8 @@
  Title: Sales By Match
  Author: Cavan Ray Theiss
 
- Description:
+ Description: Given an array of integers representing the color of each sock, 
+ determine how many pairs of socks with matching colors there are.
 
  Input: a single integer n, and an array of integers
  Output: a single integer
