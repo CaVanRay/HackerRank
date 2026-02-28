53dda8b005504798d525885cda899e25b6255fa7
Cavan Theiss <cavanrt@gmail.com>
2026-02-28 16:40:15 -0600

    Update DrawingBook.cpp

--- DrawingBook.cpp
+++ DrawingBook.cpp
@@ -1,1 +1,12 @@
+/*
+ * Complete the 'pageCount' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER p
+ */
 
+int pageCount(int n, int p) {
+
+}
