89bf2fa6f7af1843684f0c416daf08b3c39c7f37
Cavan Theiss <cavanrt@gmail.com>
2026-02-28 16:45:00 -0600

    Update DrawingBook.cpp

--- DrawingBook.cpp
+++ DrawingBook.cpp
@@ -7,8 +7,10 @@ Author: Cavan Ray Theiss
 Description: we need to find and print the minimum number of pages that
 must be turned in order to arrive at requested page
 
-Input: 2 individual integers
-Output: 1 individual integer
+Input: 2 individual integers, n = numbers of pages in the book, p = the
+page we are asked to find
+Output: 1 individual integer, it will represent the number of pages that 
+must be turned to reach the requested page
 */
 
 int pageCount(int n, int p) {
