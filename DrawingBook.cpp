ba8e4e81def6ff2051a9cb1ae73f212993f00798
Cavan Theiss <cavanrt@gmail.com>
2026-02-28 16:42:30 -0600

    Update DrawingBook.cpp

--- DrawingBook.cpp
+++ DrawingBook.cpp
@@ -10,15 +10,6 @@ Input: 2 individual integers
 Output: 1 individual integer
 */
 
-/*
- * Complete the 'pageCount' function below.
- *
- * The function is expected to return an INTEGER.
- * The function accepts following parameters:
- *  1. INTEGER n
- *  2. INTEGER p
- */
-
 int pageCount(int n, int p) {
 
 }
