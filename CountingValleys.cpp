a79a7d4dd03fac02fc7acfb28cd16f7f5ce42c25
Cavan Theiss <cavanrt@gmail.com>
2026-03-01 12:38:57 -0600

    Update CountingValleys.cpp

--- CountingValleys.cpp
+++ CountingValleys.cpp
@@ -9,7 +9,7 @@ Description:
 Input: a single integer count of the steps in the provided string, and a string comprised of
 U & D, for uphill and downhill
 
-Output: returns a single integer
+Output: returns a single integer representing the number of valleys traveled through
 */
 
 int countingValleys(int steps, string path) {
