125ca184ecd9380176be7ca8f8e86aab4634797f
Cavan Theiss <cavanrt@gmail.com>
2026-03-01 12:29:17 -0600

    Update CountingValleys.cpp

--- CountingValleys.cpp
+++ CountingValleys.cpp
@@ -1,1 +1,16 @@
+//******************************************************************************************
 
+/*
+ * Complete the 'countingValleys' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER steps
+ *  2. STRING path
+*/
+
+int countingValleys(int steps, string path) {
+
+}
+
+//******************************************************************************************
