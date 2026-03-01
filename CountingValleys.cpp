1e4f778d001e292dc52e0761b2550acdd9f704fd
Cavan Theiss <cavanrt@gmail.com>
2026-03-01 12:32:15 -0600

    Update CountingValleys.cpp

--- CountingValleys.cpp
+++ CountingValleys.cpp
@@ -8,11 +8,6 @@ Description:
 
 Input: a single integer and a string
 Output: returns a single integer
-
- * The function is expected to return an INTEGER.
- * The function accepts following parameters:
- *  1. INTEGER steps
- *  2. STRING path
 */
 
 int countingValleys(int steps, string path) {
