a0e4d93e62899e2c96271571f393c050bfbb2f62
Cavan Theiss <cavanrt@gmail.com>
2026-03-02 16:34:30 -0600

    Update FormingAMagicSquare.cpp

--- FormingAMagicSquare.cpp
+++ FormingAMagicSquare.cpp
@@ -1,1 +1,10 @@
+/*
+ * Complete the 'formingMagicSquare' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts 2D_INTEGER_ARRAY s as parameter.
+ */
 
+int formingMagicSquare(vector<vector<int>> s) {
+
+}
