b0a1c0c9556c5bb366805ffaa1a673a0fea35f3f
Cavan Theiss <cavanrt@gmail.com>
2026-03-02 15:50:57 -0600

    Update CatsAndAMouse.cpp

--- CatsAndAMouse.cpp
+++ CatsAndAMouse.cpp
@@ -1,6 +1,9 @@
 //*****************************************************************
 
+/*
+Title: Cats and a Mouse
 
+*/
 
 // Complete the catAndMouse function below.
 string catAndMouse(int x, int y, int z) {
