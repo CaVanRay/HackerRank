f2823b789b52d4108b8bbc4b7e06897e3f4c57dc
Cavan Theiss <cavanrt@gmail.com>
2026-03-04 20:23:45 -0600

    Update FormingAMagicSquare.cpp

--- FormingAMagicSquare.cpp
+++ FormingAMagicSquare.cpp
@@ -17,6 +17,8 @@ int formingMagicSquare(vector<vector<int>> s) {
   // This will be a brute force solve
 
   // Calculate all 8 combinations
+
+  // Rows
   // (0,0) (0,1) (0,2)
   // (1,0) (1,1) (1,2)
   // (2,0) (2,1) (2,2)
