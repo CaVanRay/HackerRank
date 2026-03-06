0c1edc4d9cff83ad94ef6fc76f5e5bcc339d0044
Cavan Theiss <cavanrt@gmail.com>
2026-03-05 18:45:18 -0600

    Update FormingAMagicSquare.cpp

--- FormingAMagicSquare.cpp
+++ FormingAMagicSquare.cpp
@@ -18,6 +18,7 @@ int formingMagicSquare(vector<vector<int>> s) {
   // all inputs are between 1 & 9
   // when everything is correct each row, column, & diagnal should equal 15
   // and the total sum should equal 45
+  // all we are asked to return is the cost to change the current square to a perfect square
   
 }
 
