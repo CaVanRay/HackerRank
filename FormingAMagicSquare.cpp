5d45ee5b6ee4f0381e2fbfd21a8ee9e017f5aa2e
Cavan Theiss <cavanrt@gmail.com>
2026-03-06 15:56:49 -0600

    Update FormingAMagicSquare.cpp

--- FormingAMagicSquare.cpp
+++ FormingAMagicSquare.cpp
@@ -21,6 +21,8 @@ int formingMagicSquare(vector<vector<int>> s) {
   // all we are asked to return is the cost to change the current square to a perfect square
   // The cost is just another word for difference
   // so find the current sum, and subract it from the goal sum, then return the absolute value
+int total = 0
+  
   
 }
 
