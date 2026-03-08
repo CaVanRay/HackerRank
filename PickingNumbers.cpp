5e90a55a2d02ab0b85f4b3c53e2134de68da8e2c
Cavan Theiss <cavanrt@gmail.com>
2026-03-07 22:00:46 -0600

    Create PickingNumbers.cpp

---
+++ PickingNumbers.cpp
@@ -0,0 +1,19 @@
+//************************************************************************
+
+/*
+Title: Picking Numbers
+Author: Cavan Ray Theiss
+
+Description: Given an array of integers, find the longest subarray 
+where the absolute difference between any two elements is less than
+or equal to 1
+
+Input: accepts an integer array
+Output: returns an integer
+*/
+
+int pickingNumbers(vector<int> a) {
+
+}
+
+//************************************************************************
