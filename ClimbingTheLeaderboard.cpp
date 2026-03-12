51b4c717206a37a478a0da2affe6d8a298f6b2a0
Cavan Theiss <cavanrt@gmail.com>
2026-03-11 21:04:47 -0500

    Update ClimbingTheLeaderboard.cpp

--- ClimbingTheLeaderboard.cpp
+++ ClimbingTheLeaderboard.cpp
@@ -2,7 +2,9 @@
 Title: Climbing The Leaderboard
 Author: Cavan Ray Theiss
 
-Description:
+Description: given an array of highscores, and an array of current scores
+we will return an array of where each of the current scores would rank 
+compared to the list of highscores provided.
 
 Input: 2 integer arrays
 Output: 1 integer array
