8cf89f5e48e8a8754a4562d5673ca25aab955670
Cavan Theiss <cavanrt@gmail.com>
2026-03-11 21:06:20 -0500

    Update ClimbingTheLeaderboard.cpp

--- ClimbingTheLeaderboard.cpp
+++ ClimbingTheLeaderboard.cpp
@@ -7,7 +7,7 @@ we will return an array of where each of the current scores would rank
 compared to the list of highscores provided.
 
 Input: 2 integer arrays, the leaderBoard and the current game scores
-Output: 1 integer array
+Output: 1 integer array, ranking of current scores compared to leaderboard
 */
 
 vector<int> climbingLeaderboard(vector<int> ranked, vector<int> player) {
