911314ce1a4da35fa9c72acf10cbeb87419ec320
Cavan Theiss <cavanrt@gmail.com>
2026-03-11 20:53:45 -0500

    Update ClimbingTheLeaderboard.cpp

--- ClimbingTheLeaderboard.cpp
+++ ClimbingTheLeaderboard.cpp
@@ -1,1 +1,12 @@
+/*
+ * Complete the 'climbingLeaderboard' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY ranked
+ *  2. INTEGER_ARRAY player
+ */
 
+vector<int> climbingLeaderboard(vector<int> ranked, vector<int> player) {
+
+}
