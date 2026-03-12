9536ba3b18e986e1dc316e0584d12fedfb623763
Cavan Theiss <cavanrt@gmail.com>
2026-03-11 20:58:05 -0500

    Update ClimbingTheLeaderboard.cpp

--- ClimbingTheLeaderboard.cpp
+++ ClimbingTheLeaderboard.cpp
@@ -6,13 +6,6 @@ Description:
 
 Input: 2 integer arrays
 Output: 1 integer array
-
- * Complete the 'climbingLeaderboard' function below.
- *
- * The function is expected to return an INTEGER_ARRAY.
- * The function accepts following parameters:
- *  1. INTEGER_ARRAY ranked
- *  2. INTEGER_ARRAY player
 */
 
 vector<int> climbingLeaderboard(vector<int> ranked, vector<int> player) {
