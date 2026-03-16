f51238ef23a40516a33357fcdea7595c8ee7782d
Cavan Theiss <cavanrt@gmail.com>
2026-03-15 19:47:37 -0500

    Update ClimbingTheLeaderboard.cpp

--- ClimbingTheLeaderboard.cpp
+++ ClimbingTheLeaderboard.cpp
@@ -12,6 +12,7 @@ Output: 1 integer array, ranking of current scores compared to leaderboard
 
 vector<int> climbingLeaderboard(vector<int> ranked, vector<int> player) {
   vector<int> rankings;
+  int currentRank;
   for(int currentScore : player){
     
   }
