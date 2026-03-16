c0bb8aae53f9216119cc2d6bad3b5cdeefb74c34
Cavan Theiss <cavanrt@gmail.com>
2026-03-16 12:32:24 -0500

    Update ClimbingTheLeaderboard.cpp

--- ClimbingTheLeaderboard.cpp
+++ ClimbingTheLeaderboard.cpp
@@ -29,7 +29,6 @@ vector<int> climbingLeaderboard(vector<int> ranked, vector<int> player) {
       if((previousScore > currentScore) && (previousScore != lastScore)){
         currentRank++;
       }
-      
     }
   }
 }
