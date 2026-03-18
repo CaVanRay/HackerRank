18ca4ac38ec76d09f1b37afc78d6a88c86f81b24
Cavan Theiss <cavanrt@gmail.com>
2026-03-17 21:21:22 -0500

    Update ClimbingTheLeaderboard.cpp

--- ClimbingTheLeaderboard.cpp
+++ ClimbingTheLeaderboard.cpp
@@ -15,7 +15,7 @@ vector<int> climbingLeaderboard(vector<int> ranked, vector<int> player) {
   vector<int> rankings;
   // the rankings vector is for saving the new ranks for the player scores
   
-  int currentRank, lastScore = 0;
+  int currentRank, lastScore;
   // currentRank is for iterating the current player score to their new rank
   // and the lastScore integer is for making sure duplicate scores dont get
   // counted as 2 seperate ranks
@@ -23,6 +23,7 @@ vector<int> climbingLeaderboard(vector<int> ranked, vector<int> player) {
   for(int currentScore : player){
     // for each player score that needs to be ranked
     currentRank = 1;
+    lastScore = 0;
     for(int previousScore : ranked){
       // we then run through every rank already set and 
       // compare them to each player score
