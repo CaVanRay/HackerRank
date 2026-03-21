2230cbd600dee1dc946f5423fcacb61369a9ce21
Cavan Theiss <cavanrt@gmail.com>
2026-03-21 18:38:43 -0500

    Add header comments to HurdleRace.cpp
    Added header comments with title, author, and description.

--- HurdleRace.cpp
+++ HurdleRace.cpp
@@ -1,5 +1,14 @@
 
 /*
+
 Title: The Hurdle Race
 Author: Cavan Ray Theiss
+
+Description: provided with a maximum jump height and a list
+of upcoming hurdle heights, return the number of doses needed 
+of "jump potion"
+
+Input:
+Output:
+
 */
