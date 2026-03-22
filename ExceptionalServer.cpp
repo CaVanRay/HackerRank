23461b7c3c353dc9c0a7854166fe99e169922b61
Cavan Theiss <cavanrt@gmail.com>
2026-03-22 15:26:13 -0500

    Update ExceptionalServer.cpp

--- ExceptionalServer.cpp
+++ ExceptionalServer.cpp
@@ -35,11 +35,20 @@ int main() {
 		cin >> A >> B;
 
 
-//**********************************************************************
+//******************************************************************
+//This is my code
 
-		/* Enter your code here. */
+	try{
+        cout << Server::compute(A, B) << endl;
+    } catch(const bad_alloc& e) {
+        cout << "Not enough memory" << endl;
+    } catch(const exception& e) {
+        cout << "Exception: " << e.what() << endl;
+    } catch(...) {
+        cout << "Other Exception" << endl;
+    }
 
-//**********************************************************************
+//******************************************************************
 		
 	}
 	cout << Server::getLoad() << endl;
