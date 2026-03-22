7f86999a1377299fee2bafda805548d31df90065
Cavan Theiss <cavanrt@gmail.com>
2026-03-22 15:32:41 -0500

    Update description for object creation input
    Clarified input requirements for object creation.

--- VirtualFunctions.cpp
+++ VirtualFunctions.cpp
@@ -12,7 +12,11 @@ using namespace std;
 Title: Vitual Functions
 Author: Cavan Ray Theiss
 
-Description: Depending on whether the first 
+Description: The first line of input contains the number of objects that are being created. 
+If the first line of input for each object is 1, it means that the object being created 
+is of the Professor class, you will have to input the name, age and publications of the professor.
+If the first line of input for each object is 2, it means that the object is of the Student 
+class, you will have to input the name, age and the marks of the student in  subjects.
 
 Input:
 Output:
