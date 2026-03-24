25d92ab3c1b396af55c3e4b2143d4399c1b31630
Cavan Theiss <cavanrt@gmail.com>
2026-03-23 19:12:42 -0500

    Update AbstractClassesPolymorphism.cpp

--- AbstractClassesPolymorphism.cpp
+++ AbstractClassesPolymorphism.cpp
@@ -1,3 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <string>
+#include <algorithm>
+#include <set>
+#include <cassert>
+using namespace std;
+
+struct Node{
+   Node* next;
+   Node* prev;
+   int value;
+   int key;
+   Node(Node* p, Node* n, int k, int val):prev(p),next(n),key(k),value(val){};
+   Node(int k, int val):prev(NULL),next(NULL),key(k),value(val){};
+};
+
+//************************************************************************************************
+
 /*
 
 Title: Abstract Classes Polymorphism
