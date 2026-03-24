9d7ce1631adc7ed2b193b117a6ac79434ea5f26f
Cavan Theiss <cavanrt@gmail.com>
2026-03-23 19:15:23 -0500

    Update AbstractClassesPolymorphism.cpp

--- AbstractClassesPolymorphism.cpp
+++ AbstractClassesPolymorphism.cpp
@@ -29,6 +29,7 @@ class Cache{
 };
 
 //************************************************************************************************
+// My code starts here
 
 /*
 
@@ -44,4 +45,26 @@ Output:
 
 
 
+// My code stops here
 //************************************************************************************************
+
+int main() {
+   int n, capacity,i;
+   cin >> n >> capacity;
+   LRUCache l(capacity);
+   for(i=0;i<n;i++) {
+      string command;
+      cin >> command;
+      if(command == "get") {
+         int key;
+         cin >> key;
+         cout << l.get(key) << endl;
+      } 
+      else if(command == "set") {
+         int key, value;
+         cin >> key >> value;
+         l.set(key,value);
+      }
+   }
+   return 0;
+}
