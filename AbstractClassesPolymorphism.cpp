08adcc4782356c97f2d8fc86abd9f2b1c69ebd53
Cavan Theiss <cavanrt@gmail.com>
2026-03-25 15:35:16 -0500

    Update AbstractClassesPolymorphism.cpp

--- AbstractClassesPolymorphism.cpp
+++ AbstractClassesPolymorphism.cpp
@@ -46,7 +46,10 @@ It needs to use the member functions and variables to implement an LRU cache ("L
 */
 
 class LRUCache : public Cache{
-
+   public:
+   LRUCache(int l){
+      
+   }
 };
 
 // My code stops here
