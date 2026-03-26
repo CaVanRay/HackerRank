cae3b265ac1625aedcd36e333321c7f91dc94bed
Cavan Theiss <cavanrt@gmail.com>
2026-03-26 15:56:02 -0500

    Update AbstractClassesPolymorphism.cpp

--- AbstractClassesPolymorphism.cpp
+++ AbstractClassesPolymorphism.cpp
@@ -73,6 +73,7 @@ class LRUCache : public Cache{
             } else {
                node->next = head;
                head->prev = node;
+               head = node
             }
          }
       }
