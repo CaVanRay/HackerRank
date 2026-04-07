8e6f20dba6c756c580baeb571314fd4491f460cf
Cavan Theiss <cavanrt@gmail.com>
2026-04-07 18:40:55 -0500

    Update AbstractClassesPolymorphism.cpp

--- AbstractClassesPolymorphism.cpp
+++ AbstractClassesPolymorphism.cpp
@@ -78,6 +78,7 @@ class LRUCache : public Cache{
          }else{ // This shouldnt trigger, but if somehow the list capacity hits negative an error is displayed
             cout << endl << "ERROR: Capacity not valid!" << endl; 
          }
+         mp[k] = current;
       }else if(current->key == keyID){
          
       }
