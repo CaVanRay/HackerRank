18f7f230c649aa389f47b75228854bbc49ad8f2c
Cavan Theiss <cavanrt@gmail.com>
2026-04-07 18:30:28 -0500

    Update AbstractClassesPolymorphism.cpp

--- AbstractClassesPolymorphism.cpp
+++ AbstractClassesPolymorphism.cpp
@@ -73,6 +73,7 @@ class LRUCache : public Cache{
          }else if(capacity == 0){ // Else/If the list is completely full
             head->prev = current;
             current->next = head;
+            head = current;
          }else{ // This shouldnt trigger, but if somehow the list capacity hits negative an error is displayed
             cout << endl << "ERROR: Capacity not valid!" << endl; 
          }
