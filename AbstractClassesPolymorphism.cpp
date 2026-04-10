023e99256b0efc6b646127db4e897217ce3b96ae
Cavan Theiss <cavanrt@gmail.com>
2026-04-10 16:01:00 -0500

    Update AbstractClassesPolymorphism.cpp

--- AbstractClassesPolymorphism.cpp
+++ AbstractClassesPolymorphism.cpp
@@ -94,6 +94,7 @@ class LRUCache : public Cache{
             // if the curren node is not head, not tail, we can just
             // join either side and remove the current node
             current->prev->next = current->next;
+            current->next->prev = current->prev;
          }
          // addToFront
       }else{ // This shouldnt trigger, but if somehow we don't reach the end of the list but the current item doesnt match, this will trigger
