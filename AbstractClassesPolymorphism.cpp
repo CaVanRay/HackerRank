2ee5c8786d3ac1227b43722a20c0020f026d8ea0
Cavan Theiss <cavanrt@gmail.com>
2026-04-10 15:59:40 -0500

    Update AbstractClassesPolymorphism.cpp

--- AbstractClassesPolymorphism.cpp
+++ AbstractClassesPolymorphism.cpp
@@ -89,6 +89,7 @@ class LRUCache : public Cache{
             // that it is safe to remove
             current->prev->next = NULL;
             tail = current->prev;
+            delete current;
          }else{
             // if the curren node is not head, not tail, we can just
             // join either side and remove the current node
