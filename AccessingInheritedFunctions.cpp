cb6e9e769a5e99c93281101830b9a2f8b7405ca4
Cavan Theiss <cavanrt@gmail.com>
2026-04-12 14:20:17 -0500

    Update AccessingInheritedFunctions.cpp

--- AccessingInheritedFunctions.cpp
+++ AccessingInheritedFunctions.cpp
@@ -23,20 +23,5 @@ class D : public A, public B, public C
 		 void check(int); //Do not delete this line.
 };
 
-
-
-void D::check(int new_val)
-{
-    update_val(new_val);
-    cout << "Value = " << val << endl << "A's func called " << getA() << " times " << endl << "B's func called " << getB() << " times" << endl << "C's func called " << getC() << " times" << endl;
-}
-
-
-int main()
-{
-    D d;
-    int new_val;
-    cin >> new_val;
-    d.check(new_val);
-
-}
+// My code stops here
+//************************************************************************************************
