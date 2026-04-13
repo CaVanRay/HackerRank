a5bc98ac9ef814c92fab6fbd7a3f8e70bb45ec3d
Cavan Theiss <cavanrt@gmail.com>
2026-04-13 15:09:48 -0500

    Update BC-consoleCheckerboard.cpp

--- BC-consoleCheckerboard.cpp
+++ BC-consoleCheckerboard.cpp
@@ -1,1 +1,15 @@
+#include<iostream>
 
+using namespace std;
+
+void horizontal(){
+  cout << "+";
+  while(int i = 0; i < 8; i++){
+    cout << "-----+";
+  }
+  cout << endl;
+}
+
+int main(){
+  
+}
