b6c5444815e03a7cff1db5c49b1c59ee2f7f938e
Cavan Theiss <cavanrt@gmail.com>
2026-05-11 13:53:32 -0500

    Update MagicSpells.cpp

--- MagicSpells.cpp
+++ MagicSpells.cpp
@@ -31,7 +31,9 @@ void counterspell(Spell *spell) { //********************************************
                 vector<vector<int>> vLCSMatrix(m + 1, vector<int>(n + 1));
 
                 for(int i = 1; i <= m; i++){
-                        
+                        for(int j = 1; j <= n; j++){
+                                
+                        }
                 }
         }
         
