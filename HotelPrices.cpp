432b831bbf6acae96ea803b33e1507dfbc0f9b0b
Cavan Theiss <cavanrt@gmail.com>
2026-08-26 15:20:04 -0500

    Update HotelPrices.cpp

--- HotelPrices.cpp
+++ HotelPrices.cpp
@@ -1,1 +1,10 @@
+/**********************************************
+Date: 08/26/2026
+Title: Hotel Prices
+Difficulty: Medium
+Author: Cavan Ray Theiss
 
+Description:
+
+
+**********************************************/
