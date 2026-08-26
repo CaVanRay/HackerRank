ef9e8c4c7a426c9f9d1a337a04ca0e78414c68cd
Cavan Theiss <cavanrt@gmail.com>
2026-08-26 11:13:45 -0500

    Update Deque-STL.cpp

--- Deque-STL.cpp
+++ Deque-STL.cpp
@@ -20,25 +20,8 @@ void printMaxK(int arr[], int n, int k){
 }
 
 int main() {
+
     
-    int numOfCases = 0, sizeOfDeque = 0, sizeOfSub = 0; 
-    int inputBufr, largestInSub = 0;
-    
-    std::cin >> numOfCases;
-    
-    for(int i = 0; i < numOfCases; i++){
-        
-        std::cin >> sizeOfDeque >> sizeOfSub
-        std::deque<int> mahDeque;
-        
-        for(int j = 0; j < sizeOfDeque; j++){
-            
-            std::cin >> inputBufr;
-            mahDeque.push_back(inputBufr);
-            
-        }
-        
-    }
       
     return 0;
 }
