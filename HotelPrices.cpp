6a8837b6872b39be9995db2bb3be830fcf617c7a
Cavan Theiss <cavanrt@gmail.com>
2026-08-26 15:27:14 -0500

    Update HotelPrices.cpp

--- HotelPrices.cpp
+++ HotelPrices.cpp
@@ -5,6 +5,8 @@ Difficulty: Medium
 Author: Cavan Ray Theiss
 
 Description:
-
+rather than write the code, it seems I need to 
+go through the existing code and fix whatever 
+the problem is
 
 **********************************************/
