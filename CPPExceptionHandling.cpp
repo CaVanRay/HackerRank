54b98396de26fd88ab7ed8f3492f6eb482f2ef0b
Cavan Theiss <cavanrt@gmail.com>
2026-08-27 16:27:23 -0500

    Update CPPExceptionHandling.cpp

--- CPPExceptionHandling.cpp
+++ CPPExceptionHandling.cpp
@@ -1,1 +1,9 @@
+/******************************************************************
+Date: 08/27/2026
+Title: CPP Exception Handling
+Difficulty: Medium
+Author: Cavan Ray Theiss
 
+Description:
+
+*******************************************************************/
