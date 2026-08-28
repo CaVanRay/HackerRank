6d745caab466d1e1deed5153762a6b8268e81f4c
Cavan Theiss <cavanrt@gmail.com>
2026-08-28 14:28:39 -0500

    Update CPPExceptionHandling.cpp

--- CPPExceptionHandling.cpp
+++ CPPExceptionHandling.cpp
@@ -13,3 +13,39 @@ You are required to extend the existing code so that it handles
 std::invalid_argument exception properly.
 
 *******************************************************************/
+///
+/// PROVIDED CODE BELOW
+///
+
+#include <iostream>
+#include <stdexcept>
+
+using namespace std;
+
+int largest_proper_divisor(int n) {
+    if (n == 0) {
+        throw invalid_argument("largest proper divisor is not defined for n=0");
+    }
+    if (n == 1) {
+        throw invalid_argument("largest proper divisor is not defined for n=1");
+    }
+    for (int i = n/2; i >= 1; --i) {
+        if (n % i == 0) {
+            return i;
+        }
+    }
+    return -1; // will never happen
+}
+
+void process_input(int n) {
+    int d = largest_proper_divisor(n);
+    cout << "result=" << d << endl;
+}
+
+
+int main() {
+    int n;
+    cin >> n;
+    process_input(n);
+    return 0;
+}
