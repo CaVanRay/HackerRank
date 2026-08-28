820f025c9e0bda9c0e8bef5204c34077e9d38e1a
Cavan Theiss <cavanrt@gmail.com>
2026-08-28 13:54:58 -0500

    Update CPPExceptionHandling.cpp

--- CPPExceptionHandling.cpp
+++ CPPExceptionHandling.cpp
@@ -4,6 +4,7 @@ Title: CPP Exception Handling
 Difficulty: Medium
 Author: Cavan Ray Theiss
 
-Description:
+Description: In this challenge, the task is to debug the existing 
+code to successfully execute all provided test files.
 
 *******************************************************************/
