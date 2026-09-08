ac5143b32b5d8ea45360a0333f490440fe5487bc
Cavan Theiss <cavanrt@gmail.com>
2026-09-07 19:30:14 -0500

    Update OverloadingOstreamOperator.cpp
    7

--- OverloadingOstreamOperator.cpp
+++ OverloadingOstreamOperator.cpp
@@ -7,6 +7,7 @@ Description:
 The task is to overload the << operator for Person class in such a 
 way that for p being an instance of class Person the result of:
 
+std::cout << p << " " << <some_string_value> << std::endl;
 
 
 ******************************************************************/
