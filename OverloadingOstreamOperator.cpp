39d3da4f401699a96cd6487d48f836d2c7af0002
Cavan Theiss <cavanrt@gmail.com>
2026-09-07 19:29:57 -0500

    Update OverloadingOstreamOperator.cpp
    6

--- OverloadingOstreamOperator.cpp
+++ OverloadingOstreamOperator.cpp
@@ -2,5 +2,11 @@
 Date: 9/6/26
 Title: Overloading Ostream Operators
 Author: Cavan Ray Theiss
-Description:
+Description: 
+
+The task is to overload the << operator for Person class in such a 
+way that for p being an instance of class Person the result of:
+
+
+
 ******************************************************************/
